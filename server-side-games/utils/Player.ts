a6ee0993fbab068677b34c85f26c76bfc936218e
Filip Seles <filip.seles@gmail.com>
2025-02-18 17:09:11 +0100

    refactor: renaming class from client to Player

---
+++ server-side-games/utils/Player.ts
@@ -0,0 +1,11 @@
+export class Player
+{
+	readonly id: string; 
+	readonly connection: WebSocket
+	
+	constructor(playerId: string, socket:WebSocket)
+	{
+		this.id = playerId;
+		this.connection = socket
+	}
+}
