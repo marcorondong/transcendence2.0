1b02fe7b63ee03315dbdab714e12e1a3a54ebbc6
Filip Seles <filip.seles@gmail.com>
2025-02-18 12:16:07 +0100

    client communication

--- rock-paper-scissors-api/src/index.ts
+++ rock-paper-scissors-api/src/index.ts
@@ -6,6 +6,18 @@ import websocket from "@fastify/websocket"
 const PORT:number = 3000
 const HOST:string = "0.0.0.0"
 const fastify = Fastify( {logger:true});
+const clients: Set<WebSocket> = new Set(); //set is just array but uniue values
+
+function broadcastToAll(allClients: Set<WebSocket>, sender:WebSocket, message:string)
+{
+  for(const oneClient of clients)
+  {
+    if(oneClient !== sender && oneClient.readyState === WebSocket.OPEN)
+    {
+      oneClient.send(`Client says ${message}`)
+    }
+  }
+}
 
 
 // Register WebSocket plugin
@@ -14,13 +26,16 @@ fastify.register(async function (fastify)
 {
   // fastify.get("/", { websocket: true }, handleWebSocketConnection);
   // fastify.get("/ws/", {websocket:true}, anotherHandel);
-  fastify.get("/wss", {websocket:true}, (connection, req) =>
+  fastify.get("/ws", {websocket:true}, (connection, req) =>
   {
     console.log("Client connectd");
+    clients.add(connection);
     connection.on("message", (message)=>
     {
+      const messageStr = message.toString();
+      broadcastToAll(clients, connection, messageStr);
       console.log("Received:", message.toString());
-      connection.send(`Echo: ${message}`)
+      //connection.send(`Echo: ${message}`)
     })
 
     connection.on("close", (code:number, reason:Buffer) =>
