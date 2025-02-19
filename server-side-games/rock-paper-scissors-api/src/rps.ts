2ab6e87ffcae163552764ed3533ea40aa3c98e2d
Filip Seles <filip.seles@gmail.com>
2025-02-19 14:54:31 +0100

    feat: add string to RPS shape converter, added invalid move, improve getWinner function to determine winner if invalid move happen

--- server-side-games/rock-paper-scissors-api/src/rps.ts
+++ server-side-games/rock-paper-scissors-api/src/rps.ts
@@ -1,6 +1,6 @@
 //rps stands for Rock paper scisors 
 
-export type RpsShape = "rock" | "paper" | "scissors";
+export type RpsShape = "rock" | "paper" | "scissors" | "invalid";
 
 function rockWin(player2: Choice): 1 | 2
 {
@@ -36,6 +36,14 @@ export class Choice
 	{
 		return this.shape;
 	}
+
+	static stringToRpsShape(move:string):RpsShape
+	{
+		const cleanedString = move.trim().toLowerCase().replace(/\s+/g, ""); //lowercase and remove all spaces
+		if(cleanedString !== "paper" && cleanedString !== "rock" && cleanedString !== "scissors")
+			return "invalid";
+		return cleanedString;
+	}
 }
 
 export class RPSGame
@@ -44,12 +52,16 @@ export class RPSGame
 	 * 
 	 * @param player1 player 1 choice
 	 * @param player2 player 2 choice
-	 * @returns 1 if player 1 win, 2 if player 2 win, 0 if it is tie
+	 * @returns 1 if player 1 win, 2 if player 2 win, 0 if it is tie, -1 if somethig went wrong
 	 */
-	static getWinner(player1: Choice, player2: Choice):number
+	static getWinner(player1: Choice, player2: Choice): 1 | 2 | 0 | -1
 	{
 		if(player1.getChoice() === player2.getChoice())
 			return 0;
+		if(player1.getChoice() === "invalid")
+			return 2;
+		if(player2.getChoice() === "invalid")
+			return 1;
 		const p1Move:RpsShape = player1.getChoice();
 		switch(p1Move)
 		{
@@ -60,5 +72,7 @@ export class RPSGame
 			case "scissors":
 				return scissorsWin(player2);
 		}
+		console.error("Edge case that is not covered in getWinner function. Should not be printed");
+		return -1;
 	}
 }
