9d770a3e85e55911fdd2c0aaa5e12daf7a324ed9
Filip Seles <filip.seles@gmail.com>
2025-02-19 15:49:20 +0100

    feat: refactor logic

--- server-side-games/rock-paper-scissors-api/src/index.ts
+++ server-side-games/rock-paper-scissors-api/src/index.ts
@@ -1,98 +1,72 @@
 import Fastify, { FastifyRequest } from 'fastify';
-import { WebSocket } from 'ws';
 import websocket from "@fastify/websocket"
-import { Socket } from 'dgram';
 import {Player} from "../../utils/Player"
+import {RpsRoom} from "./RpsRoom"
+import { RawData } from 'ws';
 
 
 const PORT:number = 3000
 const HOST:string = "0.0.0.0"
 let generateId = 0;
-const CLOSE_CODE:number = 1000
 const fastify = Fastify( {logger:true});
-const clients: Set<WebSocket> = new Set(); //set is just array but uniue values
 
-const players: Map<number, WebSocket> = new Map();
-
-function broadcastToAll(allClients: Map<number, WebSocket>, sender:number, message:string)
-{
-  for(const [id, oneClient] of allClients)
-  {
-    if(id !== sender && oneClient.readyState === WebSocket.OPEN)
-    {
-      oneClient.send(`Client ${id} says ${message}`)
-    }
-  }
-}
-
-function sendPrivateMessage(senderId: number, receiverId: number, message:string)
-{
-  const reciverSocket = players.get(receiverId);
-  if(reciverSocket && reciverSocket.readyState === WebSocket.OPEN && receiverId!==senderId)
-  {
-    reciverSocket.send(`Private from ${senderId}: ${message}`);
-  }
-}
-
+const oneRoom:RpsRoom = new RpsRoom("1");
 
 // Register WebSocket plugin
 fastify.register(websocket);
 fastify.register(async function (fastify)
 {
-  // fastify.get("/", { websocket: true }, handleWebSocketConnection);
-  // fastify.get("/ws/", {websocket:true}, anotherHandel);
   fastify.get("/ws", {websocket:true}, (connection, req) =>
   {
     const clienId = generateId++;
     const player1:Player = new Player(clienId.toString(), connection);
     console.log(`Loggin player info ${player1.id}`)
-    console.log(`Client connectd ${clienId}`);
-    connection.send(`Hello client ${clienId}`);
-    //clients.add(connection);
-    players.set(clienId, connection);
-    //connection.close(CLOSE_CODE, "Necu te")
-    connection.on("message", (message)=>
+
+    addPlayerToRoom(oneRoom, player1)
+
+    connection.on("message", (message:RawData)=>
     {
-      const messageStr = message.toString();
-      console.log("Received:", message.toString());
-      
-      try 
-      {
-        const data = JSON.parse(messageStr);
-        if(data.type === 'private' && data.to)
-        {
-          sendPrivateMessage(clienId, data.to, data.message);
-        }
-        else
-        {
-          broadcastToAll(players, clienId, messageStr);
-        }
-      }
-      catch(error)
-      {
-        connection.send('Invalid message format. Send JSON with type, message, and optional to.');
-      }
-      //connection.send(`Echo: ${message}`)
+      onMessageHandler(message, oneRoom, player1);
     })
 
     connection.on("close", (code:number, reason:Buffer) =>
     {
       console.log(`Client ${clienId} disconnected`);
       console.log(`Reason ${code} buffer: ${reason}`)
-      players.delete(clienId);
+      oneRoom.removePlayer(player1);
     })
   })
 
-  fastify.get("/normal", (request, reply) =>
-  {
-    reply.send(
-      {
-        hello: "good"
-      }
-    )
-  })
-
+  // fastify.get("/normal", (request, reply) =>
+  // {
+  //   reply.send(
+  //     {
+  //       hello: "good"
+  //     }
+  //   )
+  // })
 });
+
+function addPlayerToRoom(rpsRoom:RpsRoom, player:Player): void
+{
+  const successJoin:boolean = rpsRoom.addPlayer(player); 
+  if(successJoin === false)
+  {
+    player.connection.send("Cannot join full room");
+    player.connection.close(1000, "Full room");
+  }
+  rpsRoom.greetPlayer(player);
+  if(rpsRoom.isFull())
+    rpsRoom.roomBroadcast("Ready to play");
+}
+
+function onMessageHandler(message:RawData, room:RpsRoom, player:Player )
+{
+    const messageStr = message.toString();
+    console.log("Received:", message.toString());
+    room.storeMove(player, messageStr);
+    room.announceWinner();
+}
 
 const startServer = async() =>
 {
