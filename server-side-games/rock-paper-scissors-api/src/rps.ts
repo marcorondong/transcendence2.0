dee0d1cb595f0fdd4385f6f5eb9d14591e864f45
Filip Seles <filip.seles@gmail.com>
2025-02-19 15:05:47 +0100

    feat: add getWinnerAnnouncemenet function that return string who won

--- server-side-games/rock-paper-scissors-api/src/rps.ts
+++ server-side-games/rock-paper-scissors-api/src/rps.ts
@@ -52,9 +52,9 @@ export class RPSGame
 	 * 
 	 * @param player1 player 1 choice
 	 * @param player2 player 2 choice
-	 * @returns 1 if player 1 win, 2 if player 2 win, 0 if it is tie, -1 if somethig went wrong
+	 * @returns 1 if player 1 win, 2 if player 2 win, 0 if it is tie
 	 */
-	static getWinner(player1: Choice, player2: Choice): 1 | 2 | 0 | -1
+	static getWinner(player1: Choice, player2: Choice): 1 | 2 | 0
 	{
 		if(player1.getChoice() === player2.getChoice())
 			return 0;
@@ -72,7 +72,25 @@ export class RPSGame
 			case "scissors":
 				return scissorsWin(player2);
 		}
-		console.error("Edge case that is not covered in getWinner function. Should not be printed");
-		return -1;
+		throw Error("Edge case that is not covered in getWinner function. Should not be printed");
+	}
+
+	static getWinnerAnnouncement(player1: Choice, player2: Choice):string
+	{
+		const winner = this.getWinner(player1, player2);
+		let annoucement; 
+		switch (winner)
+		{
+			case 1:
+				annoucement = `Winner is player 1. ${player1.getChoice} wins over ${player2.getChoice}`
+				break;
+			case 2:
+				annoucement = `Winner is player 2. ${player2.getChoice} wins over ${player1.getChoice}`
+				break;
+			case 0:
+				annoucement = `It is draw. ${player1.getChoice} vs ${player2.getChoice}`
+				break;
+		}
+		return annoucement;
 	}
 }
