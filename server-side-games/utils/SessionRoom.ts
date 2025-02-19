f58cd90cab7426be6d63f5c859d0f26f91718742
Filip Seles <filip.seles@gmail.com>
2025-02-19 12:23:47 +0100

    feat: add greetPlayer method

--- server-side-games/utils/SessionRoom.ts
+++ server-side-games/utils/SessionRoom.ts
@@ -3,11 +3,13 @@ import {Player} from "./Player"
 export class SessionRoom
 {
 	readonly id: string; 
-	private players: Set<Player>
+	private players: Set<Player>;
+	private requiredPlayers: number;
 	
-	constructor(roomId: string)
+	constructor(roomId: string, requiredPlayers:number = 2)
 	{
 		this.id = roomId;
+		this.requiredPlayers = requiredPlayers;
 		this.players = new Set<Player>();
 	}
 
@@ -19,5 +21,17 @@ export class SessionRoom
 	getPlayerCount(): number 
 	{
 		return this.players.size;
+	}
+
+	greetPlayer(player: Player): void 
+	{
+		const greeting: string = `Hello player ${player.id} welcom to room ${this.id}`;
+		var update: string; 
+		player.connection.send(greeting);
+		if(this.getPlayerCount() <= this.requiredPlayers)
+		{
+			update = `Waiting for ${this.requiredPlayers - this.getPlayerCount()} more player to join`;
+			player.connection.send(update);
+		}
 	}
 }
