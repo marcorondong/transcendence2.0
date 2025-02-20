0c35c0e5670de7636d8e4e4f24e3287484b08927
Filip Seles <filip.seles@gmail.com>
2025-02-20 18:42:55 +0100

    feat: send very stupid hardcode frame in json to client

--- ssg/pong-api/src/index.ts
+++ ssg/pong-api/src/index.ts
@@ -1,4 +1,8 @@
 import Fastify from "fastify"
+import { Paddle } from "./Paddle";
+import { Point } from "./Point";
+import { Ball } from "./Ball";
+import { PongFrame } from "./PongFrame";
 
 const PORT:number = 3010;
 const HOST:string = "0.0.0.0"
@@ -6,15 +10,19 @@ const HOST:string = "0.0.0.0"
 
 const fastify = Fastify({logger: true});
 
+const leftPaddle: Paddle = new Paddle(new Point(-2.5, 0));
+const rightPaddle: Paddle = new Paddle(new Point(2.5, 0));
+const ball: Ball = new Ball(new Point(0, 0));
+
 fastify.register(async function(fastify)
 {
 	fastify.get("/", (request, reply) =>
 	{
-		reply.send(
-			{
-				hello: "Ping Pong server"
-			}
-		)
+		reply.send(PongFrame.getPongFrame(leftPaddle, rightPaddle, ball));
+		ball.moveBall();
+		leftPaddle.moveUp();
+		rightPaddle.moveDown();
+		rightPaddle.moveDown();
 	})
 })
 
