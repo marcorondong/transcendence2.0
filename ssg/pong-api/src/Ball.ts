c5f50a06f7c05e658fadca6e3d46d097411fcd05
Filip Seles <filip.seles@gmail.com>
2025-02-22 15:56:24 +0100

    feat: add isHit method to Ball class

--- ssg/pong-api/src/Ball.ts
+++ ssg/pong-api/src/Ball.ts
@@ -4,6 +4,7 @@ export class Ball
 {
 	protected position:Point;
 	protected vector:Point;
+	readonly radius = 0.25;
 
 	constructor(position:Point, vector:Point = new Point(0.1, 0)) 
 	{
@@ -34,5 +35,19 @@ export class Ball
 	setPosition(point:Point) :void 
 	{
 		this.position = point;
+	}
+
+	isHit(otherObjectX: number, OtherObjectY:number):boolean
+	{
+		const centerX = this.getPosition().getX();
+		const centerY = this.getPosition().getY();
+		const x = otherObjectX;
+		const y = OtherObjectY;
+
+		const result:number = Math.pow((x - centerX), 2) + Math.pow((y - centerY), 2);
+		const radiusSquared = Math.pow(this.radius, 2);
+		if(result < radiusSquared)
+			return true;
+		return false;
 	}
 }
