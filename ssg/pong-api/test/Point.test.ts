d1b6da2082fc3efac1934be6c3d019666e563943
Filip Seles <filip.seles@gmail.com>
2025-02-22 18:45:05 +0100

    test: add testes for vector movement

--- ssg/pong-api/test/Point.test.ts
+++ ssg/pong-api/test/Point.test.ts
@@ -1,4 +1,5 @@
 import { Point } from "../src/Point";
+import { VectorDirection } from "../src/Point";
 
 test('Points', () => {
 	//expect(sum(1, 2)).toBe(3);
@@ -11,3 +12,32 @@ test('Points', () => {
 	expect(same).toBe(false);
 	expect(b.getY()).toBe(1);
   });
+
+
+test("Vector movement", () => 
+{
+	const a:Point = new Point(0,0);
+	const b:Point = new Point(0,1);
+	const c:Point = new Point(0,-1);
+	
+	const d:Point = new Point(1,0);
+	const e:Point = new Point(1,1);
+	const f:Point = new Point(1,-1);
+
+	const g:Point = new Point(-1,0);
+	const h:Point = new Point(-1,1);
+	const i:Point = new Point(-1,-1);
+
+	expect(a.getMovementDirection()).toBe(VectorDirection.NO);
+	expect(b.getMovementDirection()).toBe(VectorDirection.UP);
+	expect(c.getMovementDirection()).toBe(VectorDirection.DOWN);
+
+	console.log(d.getMovementDirection());
+	expect(d.getMovementDirection()).toBe(VectorDirection.RIGHT);
+	expect(e.getMovementDirection()).toBe(VectorDirection.RIGHT_UP);
+	expect(f.getMovementDirection()).toBe(VectorDirection.RIGHT_DOWN);
+
+	expect(g.getMovementDirection()).toBe(VectorDirection.LEFT);
+	expect(h.getMovementDirection()).toBe(VectorDirection.LEFT_UP);
+	expect(i.getMovementDirection()).toBe(VectorDirection.LEFT_DOWN);
+})
