7b96c66d17f83d8a521b185e26d878d3d58f4a7c
Filip Seles <filip.seles@gmail.com>
2025-02-24 13:45:21 +0100

    test: add test for testing all 8 points of ball with postion 0.0 and radius 0.25

--- ssg/pong-api/test/Ball.test.ts
+++ ssg/pong-api/test/Ball.test.ts
@@ -1,5 +1,5 @@
 import { Ball } from "../src/Ball";
-import { Point } from "../src/Point";
+import { Point, VectorDirection } from "../src/Point";
 
 test("Ball hitbox on raidus 0.25", () =>
 {
@@ -26,5 +26,48 @@ test("Ball hitbox on raidus 0.25", () =>
 
 	missed = ball.isHit(c);
 	expect(missed).toBe(false);
+	
+})
+
+
+
+test("ball hitbox points on radius 0.25 and position 0,0", () => 
+{
+	const ball:Ball = new Ball(new Point(0,0));
+	const allPoints: Map<VectorDirection, Point> = ball.getBallHitBoxPoints();
+
+	const upPoint = allPoints.get(VectorDirection.UP);
+	const downPoint = allPoints.get(VectorDirection.DOWN);
+	const leftPoint = allPoints.get(VectorDirection.LEFT);
+	const rightPoint = allPoints.get(VectorDirection.RIGHT);
+
+	const upRight = allPoints.get(VectorDirection.RIGHT_UP);
+	const downRight = allPoints.get(VectorDirection.RIGHT_DOWN);
+	const upLeft = allPoints.get(VectorDirection.LEFT_UP);
+	const downLeft = allPoints.get(VectorDirection.LEFT_DOWN);
+
+	expect(rightPoint?.getX()).toBeCloseTo(0.25, 2);
+	expect(rightPoint?.getY()).toBeCloseTo(0, 2);
+
+	expect(upRight?.getX()).toBeCloseTo(0.18, 2);
+	expect(upRight?.getY()).toBeCloseTo(0.18, 2);
+
+	expect(upPoint?.getX()).toBeCloseTo(0, 2);
+	expect(upPoint?.getY()).toBeCloseTo(0.25, 2);
+
+	expect(upLeft?.getX()).toBeCloseTo(-0.18, 2);
+	expect(upLeft?.getY()).toBeCloseTo(0.18, 2);
+
+	expect(leftPoint?.getX()).toBeCloseTo(-0.25, 2);
+	expect(leftPoint?.getY()).toBeCloseTo(0, 2);
+
+	expect(downLeft?.getX()).toBeCloseTo(-0.18, 2);
+	expect(downLeft?.getY()).toBeCloseTo(-0.18, 2);
+
+	expect(downPoint?.getX()).toBeCloseTo(0, 2);
+	expect(downPoint?.getY()).toBeCloseTo(-0.25, 2);
+
+	expect(downRight?.getX()).toBeCloseTo(0.18, 2);
+	expect(downRight?.getY()).toBeCloseTo(-0.18, 2);
 	
 })
