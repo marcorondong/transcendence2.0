4bd1590759dcc58a6a3afb0b90a68aa934181caf
Filip Seles <filip.seles@gmail.com>
2025-02-24 12:39:56 +0100

    feat: use pino pretty in rps

--- ssg/pong-api/src/index.ts
+++ ssg/pong-api/src/index.ts
@@ -14,18 +14,22 @@ import fastifyStatic from '@fastify/static';
 const PORT:number = 3010;
 const HOST:string = "0.0.0.0"
 
-const fastify = Fastify({
-	logger: {
-		transport: {
+const fastify = Fastify(
+{
+	logger:
+	{
+		transport:
+		{
 			target: "pino-pretty",
-			options:{
+			options:
+			{
 				colorize: true, //enables colors
 				translateTime: "HH:MM:ss Z", //formating timestamt
 				ignore: "pid,hostname" //Hide fields
 			}
 		}
-
-}});
+	}
+});
 
 const leftPaddle: Paddle = new Paddle(new Point(-2.5, 0));
 const rightPaddle: Paddle = new Paddle(new Point(2.5, 0));
