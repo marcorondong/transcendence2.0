ec42b137e7045b4795ae1faf97afc713d15dc765
Filip Seles <filip.seles@gmail.com>
2025-02-27 20:07:08 +0100

    feat: securing server ignoring certificates

--- ssg/pong-api/server-keys/IMPORTANT.md
+++
@@ -1,2 +0,0 @@
-TODO:
-REMOVE SERVER KEYS FOLDER FROM FINAL PROJECT or as soon as we will have makefile that can run script for generating keys 
--- ssg/pong-api/src/index.ts
+++ ssg/pong-api/src/index.ts
@@ -17,14 +17,28 @@ dotenv.config();
 const PORT:number = 3010;
 const HOST:string = "0.0.0.0"
 
-const privateKey:string = path.join(__dirname, "../server-keys/key.pem")
-const certificate:string = path.join(__dirname, "../server-keys/cert.pem")
+const privateKeyPath:string = path.join(__dirname, "../server-keys/key.pem")
+const certificatePath:string = path.join(__dirname, "../server-keys/cert.pem")
+let privateKey: string; 
+let certificate: string;
+
+try 
+{
+	privateKey = fs.readFileSync(privateKeyPath, "utf-8");
+	certificate = fs.readFileSync(certificatePath, "utf-8");
+}
+catch
+{
+	console.error("ssl private key and certificate are not generated. Run https-key.sh script inside scripts folder first")
+	process.exit(1);
+
+}
 const fastify = Fastify(
 {
 	https:
 	{
-		key: fs.readFileSync(privateKey),
-		cert: fs.readFileSync(certificate)
+		key: privateKey,
+		cert: certificate
 	},
 
 	logger: process.env.NODE_ENV === "development"?
