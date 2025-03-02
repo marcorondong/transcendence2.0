b92309479fd48aa55b50f9cedc4b5790d24ba09e
Filip Seles <filip.seles@gmail.com>
2025-03-02 19:42:38 +0100

    fix: move frame generator sender logic from index to PongRoom class, fix issue of of multiple spectators; game was rendering frame for each client instead of generating only once and send multiple times same

--- ssg/pong-api/src/PongRoom.ts
+++ ssg/pong-api/src/PongRoom.ts
@@ -3,6 +3,8 @@ import { PingPongGame } from "./PongGame";
 import { Point } from "./Point";
 import { Paddle } from "./Paddle";
 import { Ball } from "./Ball";
+import { PongFrameI } from "./PongGame";
+import raf from "raf";
 
 
 function createGame(gameId:string) :PingPongGame
@@ -16,6 +18,8 @@ function createGame(gameId:string) :PingPongGame
 
 export class PongRoom extends SessionRoom
 {
+	private isFrameGenerating: boolean = false;
+
 	game:PingPongGame = createGame(this.id);
 	constructor(roomID: string)
 	{
@@ -25,5 +29,32 @@ export class PongRoom extends SessionRoom
 	getGame():PingPongGame
 	{
 		return this.game;
+	}
+
+
+	/**
+	 * function that make sure frame are generated only once. This fixed performance bug
+	 */
+	getAndSendFramesOnce() {
+		if(this.isFrameGenerating === false)
+		{
+			this.isFrameGenerating = true;
+			this.sendFrames();
+		}
+	}
+
+	private sendFrames()
+	{
+		const renderFrame = () => {
+			const frame: PongFrameI = this.getGame().getFrame();
+			const frameJson = JSON.stringify(frame);
+			this.roomBroadcast(frameJson)
+			if(this.getGame().isLastFrame())
+			{
+				return;
+			}
+			raf(renderFrame);
+		};
+		raf(renderFrame);
 	}
 }
