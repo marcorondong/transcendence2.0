14d3783a6449179d092c7235140ff5b9d1d63887
Filip Seles <filip.seles@gmail.com>
2025-03-05 14:23:15 +0100

    refactor: simplify pong Room methods

--- ssg/pong-api/src/PongRoom.ts
+++ ssg/pong-api/src/PongRoom.ts
@@ -1,37 +1,59 @@
 import {SessionRoom} from "../../utils/SessionRoom"
 import { PingPongGame } from "./PongGame";
-import { Point } from "./Point";
 import { Paddle } from "./Paddle";
-import { Ball } from "./Ball";
 import { PongFrameI } from "./PongGame";
 import raf from "raf";
 import { WebSocket, RawData } from "ws";
 import {Parser} from "../../utils/Parser";
-import { Player } from "../../utils/Player";
+import { PongPlayer } from "./PongPlayer";
 
 
-function createGame(gameId:string) :PingPongGame
-{
-	const leftPaddle: Paddle = new Paddle(new Point(-4, 0));
-	const rightPaddle: Paddle = new Paddle(new Point(4, 0));
-	const ball: Ball = new Ball(new Point(0, 0));
-	const game: PingPongGame = new PingPongGame(gameId, leftPaddle, rightPaddle, ball);
-	return game;
-}
 
 export class PongRoom extends SessionRoom
 {
 	private isFrameGenerating: boolean = false;
+	private readonly requiredPlayers = 2;
+	private currentPlayers = 0;
+	private leftPlayer?:PongPlayer;
+	private rightPlayer?:PongPlayer;
 
-	game:PingPongGame = createGame(this.id);
+	game:PingPongGame = PingPongGame.createStandardGame();
 	constructor(privateRoom:boolean = false)
 	{
-		super(2, privateRoom);
+		super(privateRoom);
 	}
 
 	getGame():PingPongGame
 	{
 		return this.game;
+	}
+
+	addLeftPlayer(leftPlayer: PongPlayer):boolean
+	{
+		if(this.leftPlayer === undefined)
+		{
+			this.leftPlayer = leftPlayer;
+			this.addConnectionToRoom(leftPlayer.connection);
+			this.assingControlsToPlayer(this.leftPlayer);
+			this.currentPlayers++;
+			return true
+		}
+		console.warn("Left player already exist. Cannot overwrite it")
+		return false;
+	}
+
+	addRightPlayer(rightPlayer:PongPlayer):boolean
+	{
+		if(this.rightPlayer === undefined)
+		{
+			this.rightPlayer = rightPlayer;
+			this.addConnectionToRoom(rightPlayer.connection);
+			this.assingControlsToPlayer(this.rightPlayer);
+			this.currentPlayers++;
+			return true;
+		}
+		console.warn("Right player already exist cannot overwrite it");
+		return false;
 	}
 
 
@@ -46,27 +68,36 @@ export class PongRoom extends SessionRoom
 		}
 	}
 
-	private assingControlsToPlayer(socket: WebSocket, paddleSide: "left" | "right"):void 
+	private assingControlsToPlayer(player:PongPlayer):void 
 	{
-		socket.on("message", (data: RawData, isBinnary:boolean) =>
+		player.connection.on("message", (data: RawData, isBinnary:boolean) =>
 		{
 			const json = Parser.rawDataToJson(data);
 			if(!json)
 			{
-				socket.send("Invalid json");
+				player.connection.send("Invalid json");
 				return 
 			}
 			const direction = json.move;
-			const paddle:Paddle = this.getGame().getPaddle(paddleSide);
+			const paddle:Paddle = this.getGame().getPaddle(player.getPlayerSide());
 			this.getGame().movePaddle(paddle, direction);
 		})
 	}
 
-	addAndAssingControlsToPlayer(player:Player, playerSide: "left" | "right")
+	disconnectBehaviour(rageQuitPlayer:PongPlayer)
 	{
-		this.addPlayer(player);
-		this.assingControlsToPlayer(player.connection, playerSide);
+		const socket = rageQuitPlayer.connection;
+		socket.on("close", () => {
+			console.log("PlayerLeft");
+			this.game.forfeitGame(rageQuitPlayer.getPlayerSide());
+		})
 	}
+
+	// addAndAssingControlsToPlayer(PongPlayer:PongPlayer, playerSide: "left" | "right")
+	// {
+	// 	this.addPlayer(player);
+	// 	this.assingControlsToPlayer(player.connection, playerSide);
+	// }
 
 	private sendFrames()
 	{
