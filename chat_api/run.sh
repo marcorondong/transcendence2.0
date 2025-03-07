37a597bbe162f35278235f7a862aa34cc5d012df
ismayilguliyev28 <guliyevismayil@gmail.com>
2025-03-07 14:45:18 +0100

    run.sh is added to install dependencies. To start program use: npm run dev

---
+++ chat_api/run.sh
@@ -0,0 +1,10 @@
+
+npm install --save-dev typescript @types/node
+npm install fastify @fastify/websocket @fastify/static
+npm install --save-dev nodemon
+npm install uuid
+npm install --save-dev @types/uuid
+npm install chalk
+npm install --save-dev @types/chalk
+npm install ws
+npm install @types/ws
