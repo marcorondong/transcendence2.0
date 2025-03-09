f3b377e649ac90bec9eb64cb9c58129b165e4182
Marco Rondon <marcorondong@hotmail.com>
2025-03-09 02:25:24 +0100

    [FEAT] Add main application script (app.ts)

---
+++ microservices/users2/src/app.ts
@@ -0,0 +1,22 @@
+// console.log("hello world");
+import Fastify from "fastify";
+import userRoutes from "./modules/user/user.route";
+
+const server = Fastify();
+
+server.get('/healthcheck', async function() {
+	return {status: "OK"};
+});
+
+async function main() {
+	server.register(userRoutes, {prefix: 'api/users'})
+	try{
+		await server.listen( {port: 3000, host: "0.0.0.0"});
+		console.log('Server ready at http://localhost:3000');
+	} catch(e) {
+		console.error(e);
+		process.exit(1);
+	}
+}
+
+main()
