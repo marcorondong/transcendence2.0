38479666f43cfa3a88b49b2ae91c66249f99f424
Sekula34 <filip.seles@gmail.com>
2025-03-11 15:48:28 +0100

    feat: create file for custom Events

---
+++ ssg/pong-api/src/customEvents.ts
@@ -0,0 +1,9 @@
+//File for definig all custom Events. So it is easier to define listeners
+
+
+export const TournamentEvents =
+{
+	FULL: Symbol("Tournament is full"),
+	STARTED: Symbol("Tournament is started"),
+	FINISHED: Symbol("Tournament is finished")
+}
