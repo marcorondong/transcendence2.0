91a99ed962577e1b232f789cdd4c5c1d066d5d34
ismayilguliyev28 <139191169+ismayilguliyev28@users.noreply.github.com>
2025-03-16 13:16:36 +0000

    swagger is added

--- test/src/server.ts
+++ test/src/server.ts
@@ -1,5 +1,9 @@
 import myFastify, { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 import { itemRoutes } from './routes/routes';
+import cors from '@fastify/cors';
+import { swaggerOptions, swaggerUiOptions } from './swagger/swagger';
  
 
 const PORT = 5000;
@@ -7,6 +11,16 @@ const PORT = 5000;
 const fastifyApi: FastifyInstance = myFastify({
     logger: false
 });
+
+// Allow all origins (adjust for security)
+fastifyApi.register(cors, {
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  });
+
+fastifyApi.register(fastifySwagger, swaggerOptions);
+
+fastifyApi.register(fastifySwaggerUi, swaggerUiOptions);
 
 fastifyApi.register(itemRoutes);
 
