23442c17128f207931541be411b554430d58b341
Sekula34 <filip.seles@gmail.com>
2025-03-16 01:02:03 +0100

    feat: spectator join

--- ssg/pong-api/src/MatchMaking.ts
+++ ssg/pong-api/src/MatchMaking.ts
@@ -4,6 +4,7 @@ import { GameRoomQueryI } from ".";
 import { WebSocket } from "ws";
 import { PongPlayer } from "./PongPlayer";
 import { TValidTournamentSize } from "./Tournamnet";
+import { PongRoom } from "./PongRoom";
 
 
 export class MatchMaking 
@@ -21,7 +22,8 @@ export class MatchMaking
 	{
 		if(querry.clientType === "player")
 			this.playerJoiner(connection, querry.matchType,querry.tournamentSize);
-		
+		else if(querry.clientType === "spectator")
+			this.spectatorJoiner(connection, querry.roomId);
 	}
 
 	private playerJoiner(connection: WebSocket,matchType: "single" | "tournament", tournamentSize:TValidTournamentSize)
@@ -36,4 +38,32 @@ export class MatchMaking
 			this.tournamentManager.putPlayerInTournament(player, tournamentSize);
 		}
 	}
+	
+	private spectatorJoiner(connection: WebSocket, roomId:string | 0)
+	{
+		if(roomId === 0)
+		{
+			connection.send("roomId is required querry if you are spectator")
+			connection.close();
+			return;
+		}
+		const allRooms: Map<string, PongRoom> = this.getAllActiveRooms();
+		const roomToWatch = allRooms.get(roomId);
+		if(roomToWatch != undefined)
+		{
+			roomToWatch.addSpectator(connection);
+		}
+		else 
+		{
+			connection.send(`Room with id ${roomId} was not found`);
+			return
+		}
+	}
+
+	private getAllActiveRooms(): Map<string, PongRoom>
+	{
+		const allRooms: Map<string, PongRoom> = new Map<string, PongRoom>([...this.singlesManger.getAllMatches(),
+			...this.tournamentManager.getMatchesFromAllTournaments()]);
+		return allRooms;
+	}	
 }
