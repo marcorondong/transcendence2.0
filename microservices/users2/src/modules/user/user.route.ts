16fdbe2788e48f55c597d54829e86d3aa5d8e045
Marco Rondon <marcorondong@hotmail.com>
2025-03-18 22:18:49 +0100

    [CHORE]: Removed some comments/old code in user.route.ts

--- microservices/users2/src/modules/user/user.route.ts
+++ microservices/users2/src/modules/user/user.route.ts
@@ -1,52 +1,12 @@
 import { FastifyInstance } from "fastify";
 import { registerUserHandler } from "./user.controller";
-
-// import { createUserSchema, createUserResponseSchema } from "./user.schema";
-// import { ZodTypeProvider } from "fastify-type-provider-zod";
-
-// async function userRoutes(server:FastifyInstance) {
-// 	server.withTypeProvider().post("/", {
-// 		schema: {
-// 			body: createUserSchema,
-// 			response: {
-// 				201: createUserResponseSchema,
-// 			},
-// 		}
-// 	}, registerUserHandler);
-// }
-
-// import { ZodTypeProvider } from 'fastify-type-provider-zod';  // It seems that this is not used
 import { createUserSchema, createUserResponseSchema } from './user.schema';
-
-// =================================================================
-// server.withTypeProvider<ZodTypeProvider>().post("/", {...});
-// =================================================================
-
-// =================================================================
-// async function userRoutes(server: FastifyInstance) {
-// 	const app = server.withTypeProvider<ZodTypeProvider>();
-  
-// 	app.post(
-// 		'/',
-// 		{
-// 		schema: {
-// 			body: createUserSchema,
-// 			response: {
-// 			201: createUserResponseSchema,
-// 			},
-// 		},
-// 	},
-// 	registerUserHandler
-// 	);
-// }
-// =================================================================
+// import { ZodTypeProvider } from 'fastify-type-provider-zod';  // It seems that this is not used
 
 async function userRoutes(server: FastifyInstance) {
-
 	// MR_NOTE: I should do this if I haven't enabled Zod globally
 	// const app = server.withTypeProvider<ZodTypeProvider>();
-	// server.withTypeProvider<ZodTypeProvider>().post("/", {...});
-  
+	// OR: server.withTypeProvider<ZodTypeProvider>().post("/", {...});
 	server.post(
 		"/",
 		{
@@ -60,9 +20,5 @@ async function userRoutes(server: FastifyInstance) {
 	registerUserHandler
 	);
 }
-
-// async function userRoutes(server:FastifyInstance) {
-// 	server.post("/", registerUserHandler);
-// }
 
 export default userRoutes
