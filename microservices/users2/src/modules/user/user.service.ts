22ff7c5dc156ef444dcbea07ce44d01ed6fbf317
Marco Rondon <marcorondong@hotmail.com>
2025-03-19 23:01:00 +0100

    [CHORE]: Added getUsersHandler response fields filtering (no returning salt nor password) via prisma (in user.service.ts where I manage prisma + user)

--- microservices/users2/src/modules/user/user.service.ts
+++ microservices/users2/src/modules/user/user.service.ts
@@ -20,5 +20,11 @@ export async function findUserByEmail(email: string) {
 }
 
 export async function findUsers() {
-	return prisma.user.findMany();
+	return prisma.user.findMany( {
+		select: {
+			email: true,
+			name: true,
+			id: true,
+		},
+	});
 }
