c1a698c4c2bfa4d2c0f4edd04726905420eec22d
ismayilguliyev28 <guliyevismayil@gmail.com>
2025-03-20 22:10:55 +0100

    draft

---
+++ draft/draft.md
@@ -0,0 +1,5 @@
+
+
+npm init
+npx tsc --init
+
---
+++ draft/run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo -e "\e[32mtypescript\e[0m"
+npm i -D typescript
+echo -e "\e[32mts-node\e[0m"
+npm i -D ts-node
+echo -e "\e[32mts-node\e[0m"
+npm i -D @types/node
+echo -e "\e[32mtsx\e[0m"
+npm i -D tsx
+echo -e "\e[32mfastify\e[0m"
+npm i fastify
+echo -e "\e[32mcookie\e[0m"
+npm i @fastify/cookie
+echo -e "\e[32mjwt\e[0m"
+npm i @fastify/jwt
+echo -e "\e[32mswagger\e[0m"
+npm i -D @fastify/swagger
+echo -e "\e[32mswagger-ui\e[0m"
+npm i -D @fastify/swagger-ui
+echo -e "\e[32mzod\e[0m"
+echo -e "\e[32mbcrypt\e[0m"
+npm install fastify-bcrypt
+npm i zod
+echo -e "\e[32mzod-to-json-schema\e[0m"
+npm i zod-to-json-schema
+echo -e "\e[32mfastify-type-provider-zod\e[0m"
+npm i fastify-type-provider-zod
+echo -e "\e[32mprisma\e[0m"
+npm i -D prisma
+echo -e "\e[32mprisma-client\e[0m"
+npm i @prisma/client
+echo -e "\e[32mfaker\e[0m"
+npm i -D @faker-js/faker
+echo -e "\e[32mnodemnon\e[0m"
+npm i -D nodemon
