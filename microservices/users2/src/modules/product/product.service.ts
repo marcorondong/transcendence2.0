f8a50ac9c914cbbddbde127f85201c06cabbdc62
Marco Rondon <marcorondong@hotmail.com>
2025-03-20 23:12:01 +0100

    [FIX]: Adding getProducts missing fields createdAt and updatedAt

--- microservices/users2/src/modules/product/product.service.ts
+++ microservices/users2/src/modules/product/product.service.ts
@@ -8,14 +8,15 @@ export async function createProduct(data: createProductInput & {ownerId: number}
 }
 
 // TODO: Research why this one is not async?
-// TODO: Change this to use schemas
 export function getProducts() {
 	return prisma.product.findMany({
 		select: {
-			content: true,
-			title: true,
-			price: true,
 			id: true,
+			title: true,
+			content: true,
+			price: true,
+			createdAt: true,
+			updatedAt: true,
 			owner: {
 				select: {
 					name: true,
