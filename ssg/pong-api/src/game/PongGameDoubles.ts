3d5a5485ce8b14a5b885331729a3b9789fc2610f
Sekula34 <filip.seles@gmail.com>
2025-03-20 16:32:19 +0100

    feat: some setup for game doubles

---
+++ ssg/pong-api/src/game/PongGameDoubles.ts
@@ -0,0 +1,32 @@
+import { PongGame } from "./PongGame"
+import { Paddle } from "./Paddle"
+import { Ball } from "./Ball"
+import { PongField } from "./PongField"
+import { ScoreBoard } from "./ScoreBoard"
+import { Point } from "./Point"
+
+export class PongGameDoubles extends PongGame
+{
+	private leftPaddletwo: Paddle;
+	private rightPaddletwo: Paddle;
+
+	constructor(leftPaddle: Paddle, leftTwo: Paddle, rightPaddle: Paddle, rightTwo:Paddle, ball:Ball, score:ScoreBoard, tableField:PongField)
+	{
+		super(leftPaddle, rightPaddle, ball, score, tableField);
+		this.leftPaddletwo = leftTwo;
+		this.rightPaddletwo = rightTwo;
+	}
+
+	static createStandardGameDobules(): PongGameDoubles
+	{
+		const leftPaddle: Paddle = new Paddle(new Point(-4, 1.25), 0.5);
+		const leftPaddletwo: Paddle = new Paddle(new Point(-4, -1.25), 0.5);
+		const rightPaddle: Paddle = new Paddle(new Point(4, 1.25));
+		const rightPaddletwo: Paddle = new Paddle(new Point(4, -1.25), 0.5);
+		const ball: Ball = new Ball(new Point(0, 0));
+		const table: PongField = new PongField();
+		const score: ScoreBoard = new ScoreBoard();
+		const game: PongGameDoubles = new PongGameDoubles(leftPaddle,leftPaddletwo ,rightPaddle, rightPaddletwo, ball, score, table);
+		return game;
+	}
+}
