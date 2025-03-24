da54be47152dd830301f38e1e7601a89fd9ff44b
ismayilguliyev28 <guliyevismayil@gmail.com>
2025-03-24 16:13:44 +0100

    https and http/2 are added

--- dummy_frontend_api/src/server.ts
+++ dummy_frontend_api/src/server.ts
@@ -1,14 +1,39 @@
 import Fastify, { FastifyInstance, FastifyReply, FastifyRequest, RouteShorthandOptions } from 'fastify'
+// import { Http2SecureServer } from 'http2';
 import fastifyWebsocket from '@fastify/websocket';
 import { Client } from './Client';
 import { onClientMessage, onClientDisconnect } from './utils';
 import fastifyStatic from '@fastify/static';
 import path from 'path';
+import fs from 'fs';
 
 const PORT = 3000;
 const HOST = '0.0.0.0';
+let privateKey: string;
+let certificate: string;
 
-const fastify: FastifyInstance = Fastify({ logger: false });
+try
+{
+	privateKey = fs.readFileSync(path.join(__dirname, '../ssl/key.pem'), 'utf8');
+	certificate = fs.readFileSync(path.join(__dirname, '../ssl/cert.pem'), 'utf8');
+}
+catch (err)
+{
+	console.log('Error reading SSL certificates');
+	console.log(err);
+	process.exit(1);
+}
+
+// const fastify: FastifyInstance<Http2SecureServer> = Fastify(
+const fastify: FastifyInstance = Fastify(
+{
+	// http2: true,
+	// https: {
+	// 	key: privateKey,
+	// 	cert: certificate
+	// },
+	logger: true 
+});
 
 fastify.register(fastifyStatic, {
 	root: path.join(__dirname, '../public'),
@@ -17,7 +42,7 @@ fastify.register(fastifyStatic, {
 
 fastify.register(fastifyWebsocket);
 
-fastify.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
+fastify.get('/', async (request, reply) => {
 	return reply.sendFile('index.html');
 });
 
