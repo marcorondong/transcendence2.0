06d1c3ef0e27a4c7e19b8864dadd65306978e1ba
Filip Seles <filip.seles@gmail.com>
2025-03-28 16:22:10 +0100

    type standard

--- ssg/pong-api/src/match-making/SingleMatchMaking.ts
+++ ssg/pong-api/src/match-making/SingleMatchMaking.ts
@@ -39,19 +39,19 @@ export class HeadToHeadMatchMaking
 
 	putPlayerinRandomRoom(player: PongPlayer)
 	{
-		const roomForPlayer:PongRoomSingles = this.findRoomToJoin(this.singleMatches, "singles");
+		const roomForPlayer: PongRoomSingles = this.findRoomToJoin(this.singleMatches, "singles");
 		this.addPlayerToRoom(player, roomForPlayer);
 	}
 
 	putPlayerinRandomRoomDoubles(player: PongPlayer)
 	{
-		const roomForPlayer:PongRoomDoubles = this.findRoomToJoin(this.doubleMatches, "doubles");
+		const roomForPlayer: PongRoomDoubles = this.findRoomToJoin(this.doubleMatches, "doubles");
 		this.addPlayerToRoom(player, roomForPlayer);
 	}
 
-	createRoomSingles(privateRoom: boolean):PongRoomSingles
+	createRoomSingles(privateRoom: boolean): PongRoomSingles
 	{
-		const freshRoom:PongRoomSingles = new PongRoomSingles(privateRoom);
+		const freshRoom: PongRoomSingles = new PongRoomSingles(privateRoom);
 		this.singleMatches.set(freshRoom.getId(), freshRoom);
 		this.lobbyMatchMonitor(freshRoom);
 		return freshRoom;
@@ -65,7 +65,7 @@ export class HeadToHeadMatchMaking
 		return freshRoom;
 	}
 
-	removeRoom(room: APongRoom<APongGame>):boolean 
+	removeRoom(room: APongRoom<APongGame>): boolean 
 	{
 		if(room instanceof PongRoomSingles)
 			return this.singleMatches.delete(room.getId());
@@ -75,7 +75,7 @@ export class HeadToHeadMatchMaking
 			throw new Error("Unknown instance of room");
 	}
 
-	getRoom(roomId:string)
+	getRoom(roomId: string): PongRoomSingles | undefined
 	{
 		return this.singleMatches.get(roomId);
 	}
@@ -88,7 +88,7 @@ export class HeadToHeadMatchMaking
 		mapOfMatches: Map<string, T>, 
 		roomType: "singles" | "doubles"): T
 	{
-		let toReturn:T | false = false;
+		let toReturn: T | false = false;
 		for(const [key, oneRoom] of mapOfMatches.entries())
 		{
 			if(oneRoom.isPrivate() === false && oneRoom.isFull() === false)
@@ -126,14 +126,14 @@ export class HeadToHeadMatchMaking
 		return;
 	}
 
-	private addPlayerToRoom(player: PongPlayer, room:APongRoom<APongGame>)
+	private addPlayerToRoom(player: PongPlayer, room: APongRoom<APongGame>)
 	{
 		const playerRole:EPlayerRole = room.getMissingPlayerRole();
 		player.setPlayerRole(playerRole)
 		room.addPlayer(player);
 	}
 
-	private cleanRoom(roomToClean:APongRoom<APongGame>)
+	private cleanRoom(roomToClean: APongRoom<APongGame>)
 	{
 		if(roomToClean.isRoomCleaned() === true)
 			return;
@@ -144,14 +144,14 @@ export class HeadToHeadMatchMaking
 		this.removeRoom(roomToClean);
 	}
 
-	private async lobbyMatchMonitor(room:APongRoom<APongGame>)
+	private async lobbyMatchMonitor(room: APongRoom<APongGame>)
 	{
 		room.setRoomCleanedStatus(false);
 		this.lobbyMonitor(room);
 		this.matchMonitor(room);
 	}
 
-	private async lobbyMonitor(room:APongRoom<APongGame>)
+	private async lobbyMonitor(room: APongRoom<APongGame>)
 	{
 		room.on(RoomEvents.EMPTY, ()=>
 		{
@@ -167,7 +167,7 @@ export class HeadToHeadMatchMaking
 		})
 	}
 
-	private async matchMonitor(room:APongRoom<APongGame>)
+	private async matchMonitor(room: APongRoom<APongGame>)
 	{
 		await room.getGame().waitForFinalWhistle();
 		console.log("matchMonitor: Game finished", room.getId());
