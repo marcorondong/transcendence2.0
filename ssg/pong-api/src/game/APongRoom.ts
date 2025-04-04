018ee5ec8a666f30198da9a3a910c68c6cb734a3
Filip Seles <filip.seles@gmail.com>
2025-04-04 16:39:04 +0200

    CHORE: applying prettier so code is now pretty like Shakira, and also all spelling mistakes are gone.

--- ssg/pong-api/src/game/APongRoom.ts
+++ ssg/pong-api/src/game/APongRoom.ts
@@ -1,7 +1,13 @@
 import { SessionRoom } from "../utils/SessionRoom";
 import { APongGame, EGameStatus } from "./modes/APongGame";
-import { IPongFrameSingles} from "./modes/singles/PongGameSingles";
-import { EPlayerRoleFiltered, PongPlayer, EPlayerStatus, ETeamSideFiltered, ETeamSide} from "./PongPlayer";
+import { IPongFrameSingles } from "./modes/singles/PongGameSingles";
+import {
+	EPlayerRoleFiltered,
+	PongPlayer,
+	EPlayerStatus,
+	ETeamSideFiltered,
+	ETeamSide,
+} from "./PongPlayer";
 import { WebSocket, RawData } from "ws";
 import { Paddle } from "./elements/Paddle";
 import { Parser } from "../utils/Parser";
@@ -10,36 +16,32 @@ import { ClientEvents } from "../customEvents";
 import raf from "raf";
 import { IPongFrameDoubles } from "./modes/doubles/PongGameDoubles";
 
-enum EPongRoomState
-{
+enum EPongRoomState {
 	LOBBY,
-	GAME
+	GAME,
 }
 
-export abstract class APongRoom<T extends APongGame> extends SessionRoom
-{
+export abstract class APongRoom<T extends APongGame> extends SessionRoom {
 	protected game: T;
 	protected isCleaned: boolean;
 	protected isFrameGenerating: boolean;
 	protected matchName: string;
 	protected readonly privateRoom: boolean;
-	protected roomState: EPongRoomState
-
+	protected roomState: EPongRoomState;
 
 	abstract broadcastLobbyUpdate(extraInfo: string): void;
 	abstract updateOthers(message: string): void;
 	abstract isFull(): boolean;
 	abstract isEmpty(): boolean;
 	abstract getMissingPlayerRole(): EPlayerRoleFiltered;
-	abstract setMissingPlayer(player: PongPlayer): void
+	abstract setMissingPlayer(player: PongPlayer): void;
 	abstract removePlayer(player: PongPlayer): void;
 	abstract getLeftCaptain(): PongPlayer;
 	abstract getRightCaptain(): PongPlayer;
 	abstract getGameFrame(): any;
-	abstract calculateMissingPlayers(): number
-	
-	constructor(privateRoom: boolean, match: T)
-	{
+	abstract calculateMissingPlayers(): number;
+
+	constructor(privateRoom: boolean, match: T) {
 		super();
 		this.game = match;
 		this.isCleaned = false;
@@ -48,136 +50,116 @@ export abstract class APongRoom<T extends APongGame> extends SessionRoom
 		this.privateRoom = privateRoom;
 		this.roomState = EPongRoomState.LOBBY;
 	}
-	
-	static createMatchStatusUpdate(notification: string)
-	{
+
+	static createMatchStatusUpdate(notification: string) {
 		return {
-			matchStatus: notification
-		}
+			matchStatus: notification,
+		};
 	}
 
-	getRoomIdJSON()
-	{
-		return{
-			roomId: this.getId()
-		}
+	getRoomIdJSON() {
+		return {
+			roomId: this.getId(),
+		};
 	}
 
-	async getRoomWinner(): Promise<PongPlayer>
-	{
+	async getRoomWinner(): Promise<PongPlayer> {
 		const winnerSide = await this.getRoomWinnerSide();
 		return this.fetchWinnerCaptain(winnerSide);
 	}
-		
-	async getRoomLoser(): Promise<PongPlayer>
-	{
+
+	async getRoomLoser(): Promise<PongPlayer> {
 		const loserSide = await this.getRoomLoserSide();
-		return this.fetchLoserCaptain(loserSide)
+		return this.fetchLoserCaptain(loserSide);
 	}
 
-
-	isPrivate(): boolean
-	{
+	isPrivate(): boolean {
 		return this.privateRoom;
 	}
 
-	setPongRoomState(state: EPongRoomState): void
-	{
+	setPongRoomState(state: EPongRoomState): void {
 		this.roomState = state;
 	}
 
-	getAndSendFramesOnce(): void
-	{
-		if(this.isFrameGenerating === false)
-		{
+	getAndSendFramesOnce(): void {
+		if (this.isFrameGenerating === false) {
 			this.isFrameGenerating = true;
 			this.sendFrames();
 		}
 	}
 
-	checkIfPlayerIsStillOnline(player: PongPlayer): void
-	{
-		if(player.getPlayerOnlineStatus() != EPlayerStatus.ONLINE)
+	checkIfPlayerIsStillOnline(player: PongPlayer): void {
+		if (player.getPlayerOnlineStatus() != EPlayerStatus.ONLINE)
 			this.game.forfeitGame(player.getTeamSideLR());
 	}
 
-	setMatchName(roundName: string): void
-	{
+	setMatchName(roundName: string): void {
 		this.matchName = roundName;
 	}
 
-	getMatchName(): string
-	{
+	getMatchName(): string {
 		return this.matchName;
 	}
 
-	getGame(): T
-	{
+	getGame(): T {
 		return this.game;
 	}
 
-	addSpectator(connection: WebSocket): void
-	{
+	addSpectator(connection: WebSocket): void {
 		this.addConnectionToRoom(connection);
 	}
 
-	isRoomCleaned(): boolean
-	{
+	isRoomCleaned(): boolean {
 		return this.isCleaned;
 	}
 
-	setRoomCleanedStatus(freshStatus: boolean): void
-	{
+	setRoomCleanedStatus(freshStatus: boolean): void {
 		this.isCleaned = freshStatus;
 	}
 
-	addPlayer(player: PongPlayer): void
-	{
+	addPlayer(player: PongPlayer): void {
 		this.setMissingPlayer(player);
 		this.addConnectionToRoom(player.connection);
-		this.assingControlsToPlayer(player, player.getPlayerPaddle(this.game));
-		this.disconnectBehaviour(player);
+		this.assignControlsToPlayer(player, player.getPlayerPaddle(this.game));
+		this.disconnectBehavior(player);
 		this.broadcastLobbyUpdate("Another player joined");
-		if(this.isFull())
-		{
+		if (this.isFull()) {
 			this.setPongRoomState(EPongRoomState.GAME);
 			this.emit(RoomEvents.FULL, this);
-		}
-		else
-		{
+		} else {
 			this.sendLobbyUpdate(player, "Welcome!");
 		}
 	}
 
-	sendCurrentFrame(): void
-	{
-		const frame: IPongFrameSingles | IPongFrameDoubles = this.getGameFrame();
-		const frameWithRoomId = {...frame, roomId: this.getId(), knockoutName: this.matchName};
+	sendCurrentFrame(): void {
+		const frame: IPongFrameSingles | IPongFrameDoubles =
+			this.getGameFrame();
+		const frameWithRoomId = {
+			...frame,
+			roomId: this.getId(),
+			knockoutName: this.matchName,
+		};
 		const frameJson = JSON.stringify(frameWithRoomId);
-		this.roomBroadcast(frameJson)
+		this.roomBroadcast(frameJson);
 	}
 
-	protected sendLobbyUpdate(player: PongPlayer, extraInfo: string): void
-	{
+	protected sendLobbyUpdate(player: PongPlayer, extraInfo: string): void {
 		const announcement = `${extraInfo} You are player [${player.getPlayerRoleString()}] wait for ${this.calculateMissingPlayers()} more player to join`;
-		const announcementJson = APongRoom.createMatchStatusUpdate(announcement);
+		const announcementJson =
+			APongRoom.createMatchStatusUpdate(announcement);
 		const roomIdJson = this.getRoomIdJSON();
-		const message = JSON.stringify(
-			{
-				...roomIdJson,
-				...announcementJson
-			}
-		);
+		const message = JSON.stringify({
+			...roomIdJson,
+			...announcementJson,
+		});
 
 		player.sendNotification(message);
 	}
 
-	private sendFrames(): void
-	{
+	private sendFrames(): void {
 		const renderFrame = () => {
 			this.sendCurrentFrame();
-			if(this.getGame().getGameStatus() === EGameStatus.FINISHED)
-			{
+			if (this.getGame().getGameStatus() === EGameStatus.FINISHED) {
 				return;
 			}
 			raf(renderFrame);
@@ -185,69 +167,54 @@ export abstract class APongRoom<T extends APongGame> extends SessionRoom
 		raf(renderFrame);
 	}
 
-	private assingControlsToPlayer(player: PongPlayer, playerPaddle: Paddle): void 
-	{
-		player.connection.on("message", (data: RawData, isBinary: boolean) =>
-		{
+	private assignControlsToPlayer(
+		player: PongPlayer,
+		playerPaddle: Paddle,
+	): void {
+		player.connection.on("message", (data: RawData, isBinary: boolean) => {
 			const json = Parser.rawDataToJson(data);
-			if(!json)
-			{
+			if (!json) {
 				player.connection.send("Invalid json");
-				return 
+				return;
 			}
 			const direction = json.move;
 			this.getGame().movePaddle(playerPaddle, direction);
-		})
+		});
 	}
 
-	private disconnectBehaviour(rageQuitPlayer: PongPlayer): void
-	{
-		rageQuitPlayer.on(ClientEvents.GONE_OFFLINE, (player: PongPlayer) =>
-		{
+	private disconnectBehavior(rageQuitPlayer: PongPlayer): void {
+		rageQuitPlayer.on(ClientEvents.GONE_OFFLINE, (player: PongPlayer) => {
 			console.log("Player disconnect");
-			if(this.game.getGameStatus() === EGameStatus.RUNNING)
-			{
-				console.log("Game was runnig. Rage quiter side lost");
+			if (this.game.getGameStatus() === EGameStatus.RUNNING) {
+				console.log("Game was running. Rage quitter side lost");
 				this.game.forfeitGame(player.getTeamSideLR());
-			}
-			else 
-			{
+			} else {
 				this.removePlayer(rageQuitPlayer);
 				this.updateOthers("Someone left");
-				if(this.isEmpty())
-					this.emit(RoomEvents.EMPTY, this);
+				if (this.isEmpty()) this.emit(RoomEvents.EMPTY, this);
 			}
-				
-		})
+		});
 	}
 
-	private async getRoomWinnerSide(): Promise<ETeamSideFiltered>
-	{
+	private async getRoomWinnerSide(): Promise<ETeamSideFiltered> {
 		await this.game.waitForFinalWhistle();
-		return this.game.getPongWinnerSide()
+		return this.game.getPongWinnerSide();
 	}
-		
-	private async getRoomLoserSide(): Promise<ETeamSideFiltered>
-	{
+
+	private async getRoomLoserSide(): Promise<ETeamSideFiltered> {
 		await this.game.waitForFinalWhistle();
 		return this.game.getPongLoserSide();
 	}
 
-	private fetchWinnerCaptain(winningSide: ETeamSideFiltered): PongPlayer
-	{
-		if(winningSide === ETeamSide.LEFT)
-			return this.getLeftCaptain();
-		else if(winningSide === ETeamSide.RIGTH)
-			return this.getRightCaptain();
+	private fetchWinnerCaptain(winningSide: ETeamSideFiltered): PongPlayer {
+		if (winningSide === ETeamSide.LEFT) return this.getLeftCaptain();
+		else if (winningSide === ETeamSide.RIGHT) return this.getRightCaptain();
 		throw new Error("Winning side undefined");
 	}
 
-	private fetchLoserCaptain(loserSide: ETeamSideFiltered): PongPlayer
-	{
-		if(loserSide === ETeamSide.LEFT)
-			return this.getLeftCaptain();
-		else if(loserSide === ETeamSide.RIGTH)
-			return this.getRightCaptain();
+	private fetchLoserCaptain(loserSide: ETeamSideFiltered): PongPlayer {
+		if (loserSide === ETeamSide.LEFT) return this.getLeftCaptain();
+		else if (loserSide === ETeamSide.RIGHT) return this.getRightCaptain();
 		throw new Error("Winning side undefined");
 	}
 }
--- ssg/pong-api/src/match-making/SingleMatchMaking.ts
+++ ssg/pong-api/src/match-making/SingleMatchMaking.ts
@@ -1,4 +1,4 @@
-import { PongRoomSingles } from "../game/modes/singles/PongRoomSingles"
+import { PongRoomSingles } from "../game/modes/singles/PongRoomSingles";
 import { EPlayerRole, PongPlayer } from "../game/PongPlayer";
 import { RoomEvents } from "../customEvents";
 import { PongRoomDoubles } from "../game/modes/doubles/PongRoomDoubles";
@@ -6,169 +6,162 @@ import { APongRoom } from "../game/APongRoom";
 import { PongGameSingles } from "../game/modes/singles/PongGameSingles";
 import { APongGame } from "../game/modes/APongGame";
 
-export class HeadToHeadMatchMaking
-{
+export class HeadToHeadMatchMaking {
 	private singleMatches: Map<string, PongRoomSingles>;
-	private doubleMatches: Map<string, PongRoomDoubles>
+	private doubleMatches: Map<string, PongRoomDoubles>;
 
-	constructor()
-	{
+	constructor() {
 		this.singleMatches = new Map<string, PongRoomSingles>();
 		this.doubleMatches = new Map<string, PongRoomDoubles>();
 	}
 
-	getAllMatches(): Map<string, APongRoom<APongGame>>
-	{
-		const joinedMap = new Map<string, APongRoom<APongGame>>([...this.singleMatches, ...this.doubleMatches]);
+	getAllMatches(): Map<string, APongRoom<APongGame>> {
+		const joinedMap = new Map<string, APongRoom<APongGame>>([
+			...this.singleMatches,
+			...this.doubleMatches,
+		]);
 		return joinedMap;
 	}
-	
-	putPlayerInPrivateRoom(player: PongPlayer, roomId: string, mode: "singles" | "doubles")
-	{
-		let privateRoom: APongRoom<APongGame> | undefined = this.getAllMatches().get(roomId);
-		if(privateRoom === undefined)
+
+	putPlayerInPrivateRoom(
+		player: PongPlayer,
+		roomId: string,
+		mode: "singles" | "doubles",
+	) {
+		let privateRoom: APongRoom<APongGame> | undefined =
+			this.getAllMatches().get(roomId);
+		if (privateRoom === undefined)
 			return this.createHostPrivateRoom(player, mode);
-		else 
-		{
-			if(privateRoom.isFull() === false)
+		else {
+			if (privateRoom.isFull() === false)
 				this.addPlayerToRoom(player, privateRoom);
-			else 
-				player.sendNotification("Private room is full");
+			else player.sendNotification("Private room is full");
 		}
 	}
 
-	putPlayerinRandomRoom(player: PongPlayer)
-	{
-		const roomForPlayer: PongRoomSingles = this.findRoomToJoin(this.singleMatches, "singles");
+	putPlayerInRandomRoom(player: PongPlayer) {
+		const roomForPlayer: PongRoomSingles = this.findRoomToJoin(
+			this.singleMatches,
+			"singles",
+		);
 		this.addPlayerToRoom(player, roomForPlayer);
 	}
 
-	putPlayerinRandomRoomDoubles(player: PongPlayer)
-	{
-		const roomForPlayer: PongRoomDoubles = this.findRoomToJoin(this.doubleMatches, "doubles");
+	putPlayerInRandomRoomDoubles(player: PongPlayer) {
+		const roomForPlayer: PongRoomDoubles = this.findRoomToJoin(
+			this.doubleMatches,
+			"doubles",
+		);
 		this.addPlayerToRoom(player, roomForPlayer);
 	}
 
-	createRoomSingles(privateRoom: boolean): PongRoomSingles
-	{
+	createRoomSingles(privateRoom: boolean): PongRoomSingles {
 		const freshRoom: PongRoomSingles = new PongRoomSingles(privateRoom);
 		this.singleMatches.set(freshRoom.getId(), freshRoom);
 		this.lobbyMatchMonitor(freshRoom);
 		return freshRoom;
 	}
 
-	createRoomDoubles(privateRoom: boolean): PongRoomDoubles
-	{
+	createRoomDoubles(privateRoom: boolean): PongRoomDoubles {
 		const freshRoom: PongRoomDoubles = new PongRoomDoubles(privateRoom);
 		this.doubleMatches.set(freshRoom.getId(), freshRoom);
 		this.lobbyMatchMonitor(freshRoom);
 		return freshRoom;
 	}
 
-	removeRoom(room: APongRoom<APongGame>): boolean 
-	{
-		if(room instanceof PongRoomSingles)
+	removeRoom(room: APongRoom<APongGame>): boolean {
+		if (room instanceof PongRoomSingles)
 			return this.singleMatches.delete(room.getId());
-		else if(room instanceof PongRoomDoubles)
+		else if (room instanceof PongRoomDoubles)
 			return this.doubleMatches.delete(room.getId());
-		else 
-			throw new Error("Unknown instance of room");
+		else throw new Error("Unknown instance of room");
 	}
 
-	getRoom(roomId: string): PongRoomSingles | undefined
-	{
+	getRoom(roomId: string): PongRoomSingles | undefined {
 		return this.singleMatches.get(roomId);
 	}
-	
-	findRoomToJoin(mapOfMatches: Map<string, PongRoomSingles>, roomType: "singles"): PongRoomSingles;
 
-	findRoomToJoin(mapOfMatches: Map<string, PongRoomDoubles>, roomType: "doubles"): PongRoomDoubles
-	
+	findRoomToJoin(
+		mapOfMatches: Map<string, PongRoomSingles>,
+		roomType: "singles",
+	): PongRoomSingles;
+
+	findRoomToJoin(
+		mapOfMatches: Map<string, PongRoomDoubles>,
+		roomType: "doubles",
+	): PongRoomDoubles;
+
 	findRoomToJoin<T extends APongRoom<PongGameSingles>>(
-		mapOfMatches: Map<string, T>, 
-		roomType: "singles" | "doubles"): T
-	{
+		mapOfMatches: Map<string, T>,
+		roomType: "singles" | "doubles",
+	): T {
 		let toReturn: T | false = false;
-		for(const [key, oneRoom] of mapOfMatches.entries())
-		{
-			if(oneRoom.isPrivate() === false && oneRoom.isFull() === false)
-			{
-				if(toReturn === false)
-					toReturn = oneRoom
-				else
-				{
-					if(oneRoom.getCreationDate() < toReturn.getCreationDate())
-						toReturn = oneRoom
+		for (const [key, oneRoom] of mapOfMatches.entries()) {
+			if (oneRoom.isPrivate() === false && oneRoom.isFull() === false) {
+				if (toReturn === false) toReturn = oneRoom;
+				else {
+					if (oneRoom.getCreationDate() < toReturn.getCreationDate())
+						toReturn = oneRoom;
 				}
 			}
 		}
 		if (toReturn === false) {
-			return (roomType === "singles" 
+			return roomType === "singles"
 				? (this.createRoomSingles(false) as unknown as T)
-				: (this.createRoomDoubles(false) as unknown as T));
+				: (this.createRoomDoubles(false) as unknown as T);
 		}
-		return toReturn
+		return toReturn;
 	}
 
-
-	private createHostPrivateRoom(host: PongPlayer, mode: "singles" | "doubles")
-	{
-		if(mode === "singles")
-		{
+	private createHostPrivateRoom(
+		host: PongPlayer,
+		mode: "singles" | "doubles",
+	) {
+		if (mode === "singles") {
 			const privateRoomForPlayer = this.createRoomSingles(true);
-			this.addPlayerToRoom(host, privateRoomForPlayer)
-		}
-		else if(mode === "doubles")
-		{
+			this.addPlayerToRoom(host, privateRoomForPlayer);
+		} else if (mode === "doubles") {
 			const privateRoomForPlayer = this.createRoomDoubles(true);
 			this.addPlayerToRoom(host, privateRoomForPlayer);
 		}
 		return;
 	}
 
-	private addPlayerToRoom(player: PongPlayer, room: APongRoom<APongGame>)
-	{
-		const playerRole:EPlayerRole = room.getMissingPlayerRole();
-		player.setPlayerRole(playerRole)
+	private addPlayerToRoom(player: PongPlayer, room: APongRoom<APongGame>) {
+		const playerRole: EPlayerRole = room.getMissingPlayerRole();
+		player.setPlayerRole(playerRole);
 		room.addPlayer(player);
 	}
 
-	private cleanRoom(roomToClean: APongRoom<APongGame>)
-	{
-		if(roomToClean.isRoomCleaned() === true)
-			return;
+	private cleanRoom(roomToClean: APongRoom<APongGame>) {
+		if (roomToClean.isRoomCleaned() === true) return;
 		roomToClean.sendCurrentFrame(); //send last frame that notify client of finished game.
 		console.log(`Clean function on room ${roomToClean.getId()}`);
 		roomToClean.setRoomCleanedStatus(true);
-		roomToClean.closeAllConecctionsFromRoom();
+		roomToClean.closeAllConnectionsFromRoom();
 		this.removeRoom(roomToClean);
 	}
 
-	private async lobbyMatchMonitor(room: APongRoom<APongGame>)
-	{
+	private async lobbyMatchMonitor(room: APongRoom<APongGame>) {
 		room.setRoomCleanedStatus(false);
 		this.lobbyMonitor(room);
 		this.matchMonitor(room);
 	}
 
-	private async lobbyMonitor(room: APongRoom<APongGame>)
-	{
-		room.on(RoomEvents.EMPTY, ()=>
-		{
+	private async lobbyMonitor(room: APongRoom<APongGame>) {
+		room.on(RoomEvents.EMPTY, () => {
 			console.log("Lobby monitor Removing empty room: ", room.getId());
 			this.cleanRoom(room);
-		})
+		});
 
-		room.on(RoomEvents.FULL, ()=>
-		{
+		room.on(RoomEvents.FULL, () => {
 			console.log(`Room ${room.getId()} is full lets get started`);
 			room.getAndSendFramesOnce();
 			room.getGame().startGame();
-		})
+		});
 	}
 
-	private async matchMonitor(room: APongRoom<APongGame>)
-	{
+	private async matchMonitor(room: APongRoom<APongGame>) {
 		await room.getGame().waitForFinalWhistle();
 		console.log("matchMonitor: Game finished", room.getId());
 		this.cleanRoom(room);
