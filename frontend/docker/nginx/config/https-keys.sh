2e78842ce306ac5b15c806bb7508e56792ae6a8e
Filip Seles <filip.seles@gmail.com>
2025-04-06 15:14:24 +0200

    just: adding nginx and Sekula in custom words. Deleted unused file

--- frontend/docker/nginx/config/https-keys.sh
+++ frontend/docker/nginx/config/https-keys.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 mkdir -p "/etc/nginx/"
-openssl req -x509 -newkey rsa:4096 -keyout "/etc/nginx/key.pem" -out "/etc/nginx/cert.pem" -sha256 -days 3650 -nodes -subj "/C=AT/ST=Austria/L=Vienna/O=Transcedence2.0/OU=VoidCluster/CN=CommonNameOrHostname"
+openssl req -x509 -newkey rsa:4096 -keyout "/etc/nginx/key.pem" -out "/etc/nginx/cert.pem" -sha256 -days 3650 -nodes -subj "/C=AT/ST=Austria/L=Vienna/O=Transcendence2.0/OU=VoidCluster/CN=CommonNameOrHostname"
