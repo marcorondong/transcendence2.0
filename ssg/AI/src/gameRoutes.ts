20b88d6200c758cacb995e2927e9f48e2748fc48
Benjamin Szilas <bszilas@student.42vienna.com>
2025-04-16 15:22:19 +0200

    feat: two routes one for mandatory AI one for extra AI

--- ssg/AI/src/gameRoutes.ts
+++ ssg/AI/src/gameRoutes.ts
@@ -1,13 +1,16 @@
 import { FastifyInstance } from "fastify";
-import { gameRequestSchema } from "./gameRequestSchema";
-import { healthSchema } from "./gameRequestSchema";
+import { 
+	gameRequestSchema,
+	extraGameSchema,
+	healthSchema,
+} from "./gameRequestSchema";
 import { Bot } from "./bot";
 
 export async function gameRoutes(fastify: FastifyInstance) {
 	fastify.post(
-		"/start-game",
-		{ schema: gameRequestSchema },
-		async (request, reply) => {
+		"/game-extra",
+		{ schema: extraGameSchema },
+		async (request: any, reply: any) => {
 			const gameRequest = request.body as object;
 			if (!gameRequest) {
 				return reply.code(400).send({ error: "Invalid game request" });
@@ -27,13 +30,42 @@ export async function gameRoutes(fastify: FastifyInstance) {
 			}
 		},
 	);
+	fastify.post(
+		"/game-mandatory",
+		{ schema: gameRequestSchema },
+		async (request: any, reply: any) => {
+			if (!request.body) {
+				return reply.code(400).send({ error: "Invalid game request" });
+			}
+			const gameRequest = new Object({
+				"host": request.body.host,
+				"port": request.body.port,
+				"side": request.body.side,
+				"difficulty": "medium",
+				"roomId": request.body.roomId,
+			});
+			try {
+				new Bot(gameRequest).playGame();
+				reply
+					.code(200)
+					.send({
+						description: "Game successfully started",
+						gameRequest,
+					});
+			} catch (error) {
+				request.log.error(error);
+				reply.code(500).send({ error: "Failed to start bot" });
+			}
+		},
+	);
 }
 
 export async function healthRoute(fastify: FastifyInstance) {
 	fastify.get(
-		'/health',
-		async (request, reply) => {
-			reply.code(200).send();
+		'/healthcheck',
+		{ schema: healthSchema },
+		async (request: any, reply: any) => {
+			reply.code(200).send({ status: "ok" });
 		}
 	);
 }
