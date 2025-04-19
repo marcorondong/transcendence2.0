e0216b704c6670cd7af2f0c00bdff921464f695f
Gabriel Danis <gabriel.danis@gmail.com>
2025-04-19 17:48:13 +0200

    [FEATURE]: setup pong component

---
+++ frontend/volume/typescript/components/pong-component.ts
@@ -0,0 +1,27 @@
+export class PongComponent extends HTMLElement {
+	constructor() {
+		super();
+	}
+
+	connectedCallback() {
+		console.log("Pong CONNECTED");
+	}
+
+	disconnectedCallback() {
+		console.log("Pong DISCONNECTED");
+	}
+}
+
+customElements.define("pong-component", PongComponent);
+
+// const queryParams = window.location.search;
+// const wss = new WebSocket(
+// 	// `ws://${window.location.hostname}:${window.location.port}/pong/${queryParams}`,
+// 	`wss://${window.location.hostname}:${window.location.port}/pong/${queryParams}`,
+// );
+
+// wss.onmessage = (event) => {
+// 	console.log("got message");
+// 	const gameState = JSON.parse(event.data);
+// 	console.log("game State", gameState);
+// };
--- frontend/volume/typescript/main.ts
+++ frontend/volume/typescript/main.ts
@@ -1,47 +1,6 @@
 import { Router } from "./router.js";
-import "./components/icon-component.js";
-import "./components/theme-toggle-component.js";
 import "./components/header-component.js";
 
 const router = new Router();
 
 router.loadComponent();
-
-const navbarButton = document.getElementById("navbar-button");
-const navbarLinks = document.getElementById("navbar-links");
-const dataTheme = document.getElementById("theme-toggle");
-
-dataTheme?.addEventListener("click", () => {
-	const theme = document.documentElement.getAttribute("data-theme");
-	if (theme) {
-		if (theme === "dark") {
-			document.documentElement.setAttribute("data-theme", "light");
-		} else {
-			document.documentElement.setAttribute("data-theme", "dark");
-		}
-	}
-});
-
-navbarButton?.addEventListener("click", () => {
-	if (navbarLinks?.classList.contains("hidden")) {
-		navbarLinks?.classList.remove("hidden");
-	} else {
-		navbarLinks?.classList.add("hidden");
-	}
-});
-
-navbarLinks?.addEventListener("click", () => {
-	navbarLinks?.classList.add("hidden");
-});
-
-// const queryParams = window.location.search;
-// const wss = new WebSocket(
-// 	// `ws://${window.location.hostname}:${window.location.port}/pong/${queryParams}`,
-// 	`wss://${window.location.hostname}:${window.location.port}/pong/${queryParams}`,
-// );
-
-// wss.onmessage = (event) => {
-// 	console.log("got message");
-// 	const gameState = JSON.parse(event.data);
-// 	console.log("game State", gameState);
-// };
