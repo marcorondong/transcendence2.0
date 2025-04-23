4a56ca51b7b4d332353d3877f1d11ad05dcace1c
ismayilguliyev28 <guliyevismayil@gmail.com>
2025-04-23 11:51:04 +0200

    route endings changed from cammel to kebab

--- chat_db/src/routes/routes.ts
+++ chat_db/src/routes/routes.ts
@@ -8,9 +8,9 @@ import {
 } from "./options";
 
 export async function chatRoutes(server: FastifyInstance) {
-	server.post("/createUser", createUserOpt);
-	server.patch("/blockUser", blockUserOpt);
-	server.patch("/unblockUser", unblockUserOpt);
-	server.get("/blockStatus/:id/:friendId", blockStatusOpt);
-	server.get("/blockList/:id", blockListOpt);
+	server.post("/create-user", createUserOpt);
+	server.patch("/block-user", blockUserOpt);
+	server.patch("/unblock-user", unblockUserOpt);
+	server.get("/block-status/:id/:friendId", blockStatusOpt);
+	server.get("/block-list/:id", blockListOpt);
 }
