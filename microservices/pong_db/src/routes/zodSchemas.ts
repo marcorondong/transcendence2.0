eb0105ee5206491e8e2a901b9d3946d2b37a8bdf
ismayilguliyev28 <guliyevismayil@gmail.com>
2025-05-03 15:12:14 +0200

    Filip's requested fields are added to DB

--- microservices/pong_db/src/routes/zodSchemas.ts
+++ microservices/pong_db/src/routes/zodSchemas.ts
@@ -13,14 +13,26 @@ export const idsZodSchema = z
 	})
 	.strict();
 
-export const gameSchema = z
-	.object({
-		winnerId: z.string().min(1, "Winner ID cannot be empty"), // TODO merge this line when id is provided in JWT: .uuid("Invalid player ID format"),
-		loserId: z.string().min(1, "Loser ID cannot be empty"), // TODO merge this line when id is provided in JWT: .uuid("Invalid player ID format"),
-	})
-	.strict();
+const baseSchema = z.object({
+	winnerId: z.string().min(1, "Winner ID cannot be empty"), // TODO merge this line when id is provided in JWT: .uuid("Invalid player ID format"),
+	loserId: z.string().min(1, "Loser ID cannot be empty"), // TODO merge this line when id is provided in JWT: .uuid("Invalid player ID format"),
+	winnerScore: z.number().int().nonnegative(),
+	loserScore: z.number().int().nonnegative(),
+});
 
-export const gameHistoryResponseSchema = z.array(gameSchema);
+export const gameSchema = baseSchema
+		.extend({
+			gameId: z.string().uuid("Invalid game ID format"),
+		})
+		.strict();
+	
+const gameResponseSchema = baseSchema
+		.extend({
+			createdAt: z.date(),
+		})
+		.strict();
+
+export const gameHistoryResponseSchema = z.array(gameResponseSchema);
 
 export const statsResponseSchema = z
 	.object({
@@ -40,4 +52,5 @@ export type IdInput = z.infer<typeof idZodSchema>;
 export type IdsInput = z.infer<typeof idsZodSchema>;
 export type GameInput = z.infer<typeof gameSchema>;
 export type GamesInput = z.infer<typeof gameHistoryResponseSchema>;
+export type GameResponse = z.infer<typeof gameResponseSchema>;
 export type StatsInput = z.infer<typeof statsResponseSchema>;
