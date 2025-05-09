5e428d86f7e9a03446f8d1af574612b678e4aa9c
ismayilguliyev28 <guliyevismayil@gmail.com>
2025-05-09 18:22:18 +0200

    Auth Microservice

--- microservices/auth_api/src/routes/routeSchemas.ts
+++ microservices/auth_api/src/routes/routeSchemas.ts
@@ -5,7 +5,7 @@ import {
 
 export const signInSchema = {
 	summary: "Sign In",
-	description: "Sign in a user",
+	description: "Sign in a user. This will create the token for the user.",
 	tags: ["Auth"],
 	body: signInZodSchema,
 	response: { 201: successResponseSchema },
@@ -13,14 +13,14 @@ export const signInSchema = {
 
 export const signOutSchema = {
 	summary: "Sign Out",
-	description: "Sign out a user",
+	description: "Sign out a user. This will delete the token for the user.",
 	tags: ["Auth"],
 	response: { 200: successResponseSchema },
 };
 
 export const verifyJWTSchema = {
 	summary: "Verify",
-	description: "Verify a user",
+	description: "Verify a user. This will check if the token is valid.",
 	tags: ["Auth"],
 	response: { 200: successResponseSchema },
 };
