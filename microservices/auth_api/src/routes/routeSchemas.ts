79861beaea92709be6360127202bf238872a0c3f
ismayilguliyev28 <guliyevismayil@gmail.com>
2025-05-09 15:35:38 +0200

    auth_api

---
+++ microservices/auth_api/src/routes/routeSchemas.ts
@@ -0,0 +1,33 @@
+import {
+	signInZodSchema,
+	successResponseSchema,
+} from "./zodSchemas";
+
+export const signInSchema = {
+	summary: "Sign In",
+	description: "Sign in a user",
+	tags: ["Auth"],
+	body: signInZodSchema,
+	response: { 201: successResponseSchema },
+};
+
+export const signOutSchema = {
+	summary: "Sign Out",
+	description: "Sign out a user",
+	tags: ["Auth"],
+	response: { 200: successResponseSchema },
+};
+
+export const verifyJWTSchema = {
+	summary: "Verify",
+	description: "Verify a user",
+	tags: ["Auth"],
+	response: { 200: successResponseSchema },
+};
+
+export const healthCheckSchema = {
+	summary: "Health Check",
+	description: "Check the health of the service",
+	tags: ["Auth"],
+	response: { 200: successResponseSchema },
+};
