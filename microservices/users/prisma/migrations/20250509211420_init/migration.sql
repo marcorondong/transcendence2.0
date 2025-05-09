3fc1a7834706ffd46d55e53ddcf6023ee4fe6eee
Marco Rondon <marcorondong@hotmail.com>
2025-05-09 23:56:19 +0200

    CHORE: Updated Prisma migrations.sql to reflect new nickname field

--- microservices/users/prisma/migrations/20250509211420_init/migration.sql
+++ microservices/users/prisma/migrations/20250509211420_init/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "User" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "email" TEXT NOT NULL,
     "username" TEXT NOT NULL,
+    "nickname" TEXT NOT NULL,
     "passwordHash" TEXT NOT NULL,
     "salt" TEXT NOT NULL
 );
@@ -27,3 +28,6 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_nickname_key" ON "User"("nickname");
