b0690341318505a3fff0be5a3776639fb329e7cf
Filip Seles <filip.seles@gmail.com>
2025-05-24 18:19:05 +0200

    feat: first version of stupid access Token

--- cli-client/menu/connect.py
+++ cli-client/menu/connect.py
@@ -1,11 +1,20 @@
 from websockets.sync.client import connect
 import json
 import curses
+import ssl
+from websockets.sync.client import connect
 
-HOST = "localhost"
-PORT = "3010"
-ROUTE = "/pong-api/pong"
-WS_ROUTE = f"ws://{HOST}:{PORT}{ROUTE}"
+HOST = "localhost" #TODO read this from proper file like 10.12.5.7
+PORT = "8080"
+ROUTE = "/pong-api/pong/singles"
+WS_ROUTE = f"wss://{HOST}:{PORT}{ROUTE}"
+ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImFlNDA0YmU5LTFjN2QtNDJiMS04ODdmLWQ4NDMyZmRlZWVkOCIsIm5pY2tuYW1lIjoic2hha2lyYSIsImlhdCI6MTc0NzU5Nzg0MSwiZXhwIjoxNzU1MzczODQxfQ.Kt_SgeRxIR39ROj2q8qbPqyN0kqNCs9RF-27h56jQDU.gZqxhwIdiuhvdnxGTFzXSVFtwA7BDpqIq8S1Rw6trUA"
+
+ssl_context = ssl._create_unverified_context() #Otherwise python will not let you bypass self signed certificate
+
+custom_headers = {
+	"Cookie": f"access_token={ACCESS_TOKEN}"
+}
 
 TAB_SIZE = 2
 CONTROLS_TUTORIAL="Use 'w' to go up with paddle, 's' to go down with paddle, 'q' to quit. Look at browser; rendering is not implemented in cli yet."
@@ -25,7 +34,7 @@ def getMove(stdscr):
 	return moveMapping.get(move, "none")
 
 def client(stdscr):
-	with connect(WS_ROUTE, close_timeout=0.1, ping_interval=None) as websocket:
+	with connect(WS_ROUTE, close_timeout=0.1, ping_interval=None, ssl=ssl_context, additional_headers=custom_headers) as websocket:
 		stdscr.addstr(0, TAB_SIZE, CONTROLS_TUTORIAL)
 		stdscr.refresh()
 		while True:
