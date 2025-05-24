45de7693aceeac75ffb7ffaa5746323446501101
Filip Seles <filip.seles@gmail.com>
2025-05-24 18:55:10 +0200

    feat: implement custom logger

--- cli-client/mainMenu.py
+++ cli-client/mainMenu.py
@@ -2,6 +2,7 @@
 from menu.drawMenu import menuLoop
 from window.windowColors import setColors
 import curses
+from menu.logger import myLogger
 
 def mainMenu(stdscr):
 	setColors(stdscr, curses.COLOR_WHITE, curses.COLOR_BLACK)
@@ -11,8 +12,10 @@ def mainMenu(stdscr):
 		"Random Game",
 		"Register",
 		"nonexistent menu option",
+		"Login Filip"
 	])
-	
+
 if __name__ == "__main__":
+	myLogger.debug("First function called")
 	curses.wrapper(mainMenu)
 		# curses.wrapper(main) initializes the screen and handles cleanup
