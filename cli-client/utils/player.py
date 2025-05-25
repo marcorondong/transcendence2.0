53edefba2479c1874168f7a01c9cf154dbc256dd
Filip Seles <filip.seles@gmail.com>
2025-05-25 11:59:39 +0200

    docs: add  Docstring to functions

--- cli-client/utils/player.py
+++ cli-client/utils/player.py
@@ -3,8 +3,14 @@ class Player:
     access_token: bool
 
     def __init__(self):
+        """Initial Player class"""
         self.username = "<GUEST>"
         self.access_token = False
 
     def set_username(self, username: str) -> None:
+        """Set username for player
+
+        Args:
+            username (str): player username
+        """
         self.username = username
--- cli-client/utils/prompt.py
+++ cli-client/utils/prompt.py
@@ -19,9 +19,23 @@ class Prompt:
     name: str
 
     def __init__(self, prompt_name: str):
+        """_summary_
+
+        Args:
+            prompt_name (str): name of prompt. Example "Username"
+        """
         self.name = prompt_name
 
     def get_prompt(self, screen: curses.window, starting_line=0) -> str:
+        """Return user prompt visible typing
+
+        Args:
+            screen (curses.window): _description_
+            starting_line (int, optional): Line number. Defaults to 0.
+
+        Returns:
+            str: user input
+        """
         curses.echo()
         prompt = f"Enter {self.name}:"
         screen.addstr(starting_line, 0, prompt)
@@ -32,6 +46,16 @@ class Prompt:
         return user_input_bytes.decode()
 
     def get_secret_prompt(self, screen: curses.window, starting_line=0) -> str:
+        """Return user prompt but it is not visible while typing.\n
+        It shows "*" instead of real input
+
+        Args:
+            screen (curses.window): _description_
+            starting_line (int, optional): _description_. Defaults to 0.
+
+        Returns:
+            str: secret input like password
+        """
         curses.noecho()
         prompt = f"Enter {self.name}:"
         screen.addstr(starting_line, 0, prompt, curses.A_STANDOUT)
