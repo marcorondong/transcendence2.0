cdaa15fb0509f68329f2d73fd3967a4626d2105d
ismayilguliyev28 <guliyevismayil@gmail.com>
2025-06-01 03:36:16 +0200

    [COMMENT] comment removed

--- microservices/auth_api/src/routes/configs.ts
+++ microservices/auth_api/src/routes/configs.ts
@@ -9,16 +9,6 @@ export const setCookieOpt = {
 
 export const jwtSignOpt = {
 	expiresIn: "1h",
-	// issuer: "auth_api_sign",
-	// iss: "auth_api",
-	// audience: "users",
-	// aud: "users",
-	// jwtid: "auth_api",
-	// keyid: "auth_api",
-	// notBefore: '10s',
-	// subject: "auth_api",
-	// algorithm: "HS256",
-	// customClaim: "customClaimValue",
 };
 
 export const clearCookieOpt = {
