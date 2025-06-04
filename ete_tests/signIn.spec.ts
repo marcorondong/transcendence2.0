01452ffc885957484f4fe1cb2d83e0dc8209e564
Filip Seles <filip.seles@gmail.com>
2025-06-04 19:16:38 +0200

    feat: code generated visibility test

--- ete_tests/signIn.spec.ts
+++ ete_tests/signIn.spec.ts
@@ -2,6 +2,10 @@ import { test, expect } from "@playwright/test";
 import { homeUrl } from "./config";
 import { ElementTest } from "./elementsTest";
 
+test.use({
+	ignoreHTTPSErrors: true,
+});
+
 test("Guest first view", async ({ page }) => {
 	await page.goto(homeUrl);
 	await expect(page).toHaveURL(`${homeUrl}/sign-in-view`);
@@ -10,4 +14,25 @@ test("Guest first view", async ({ page }) => {
 	await ElementTest.labelTest(page, "Your Username");
 	await ElementTest.labelTest(page, "Your Password");
 	await ElementTest.buttonTest(page, "Sign in");
+});
+
+test("Home page visibility test", async ({ page }) => {
+	await page.goto(homeUrl);
+	await expect(
+		page.getByRole("textbox", { name: "Your Username" }),
+	).toBeVisible();
+	await expect(
+		page.getByRole("textbox", { name: "Your Password" }),
+	).toBeVisible();
+	await expect(page.getByRole("button", { name: "Sign in" })).toBeVisible();
+	await expect(
+		page.getByRole("link", { name: "Sign up", exact: true }),
+	).toBeVisible();
+	await expect(page.getByRole("link", { name: "Sign In" })).toBeVisible();
+	await expect(
+		page.getByRole("link", { name: "Sign Up", exact: true }),
+	).toBeVisible();
+	await expect(
+		page.locator("theme-toggle-component").getByRole("button"),
+	).toBeVisible();
 });
