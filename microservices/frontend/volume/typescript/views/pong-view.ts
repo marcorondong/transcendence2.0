7b42328bf397952a604786b1c2b60a292acfee77
szilas <szbengio@gmail.com>
2025-06-07 14:41:18 +0200

    REFACTOR removed play ai-button that didnt work

--- microservices/frontend/volume/typescript/views/pong-view.ts
+++ microservices/frontend/volume/typescript/views/pong-view.ts
@@ -10,8 +10,6 @@ class PongView extends HTMLElement {
 		this.chat = chat;
 	}
 
-	playAIButton = document.createElement("button");
-
 	connectedCallback() {
 		console.log("PONG VIEW has been CONNECTED");
 
@@ -20,54 +18,6 @@ class PongView extends HTMLElement {
 		const pongComponent = new PongComponent(this.chat);
 		container.appendChild(pongComponent);
 		pongComponent.classList.add("self-center");
-
-		// Add Play Against AI button
-		this.playAIButton.innerText = "Play Against AI";
-		this.playAIButton.classList.add(
-			"pong-button",
-			"pong-button-info",
-			"mt-4",
-			"self-center",
-		);
-		container.appendChild(this.playAIButton);
-
-		// Add click event listener
-		this.playAIButton.addEventListener(
-			"click",
-			this.handlePlayAI.bind(this),
-		);
-	}
-
-	async handlePlayAI() {
-		try {
-			const roomId = this.chat.roomId;
-			const response = await fetch(
-				`https://${window.location.hostname}:${window.location.port}/ai-api/game-mandatory`,
-				{
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({
-						difficulty: "normal",
-						roomId: roomId,
-					}),
-				},
-			);
-
-			if (response.ok) {
-				console.log("Game started successfully");
-			} else {
-				console.error(
-					"Failed to start game: ",
-					response.status,
-					" ",
-					response.body,
-				);
-			}
-		} catch (error) {
-			console.error("Error starting game:", error);
-		}
 	}
 
 	disconnectedCallback() {
