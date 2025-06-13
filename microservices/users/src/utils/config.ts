54601ebe1b26291394bf7050d39e63f81c5731aa
Marco Rondon <marcorondong@hotmail.com>
2025-06-13 17:24:26 +0200

    REFACT: Refactored getConfig to use AppError, log values soirces and easier path traversal definitions

--- microservices/users/src/utils/config.ts
+++ microservices/users/src/utils/config.ts
@@ -2,6 +2,33 @@ import fs from "fs";
 import path from "path";
 import dotenv from "dotenv";
 import { logger } from "./logger";
+import { AppError, CONFIG_ERRORS } from "./errors";
+
+const ROOT_FOLDER_NAME = process.env.ROOT_FOLDER_NAME || "transcendence2.0"; // Set to "/" when running containerized
+
+function findRoot(startDir = __dirname): string {
+	if (ROOT_FOLDER_NAME === "/") return "/";
+	let current = startDir;
+	while (current !== path.parse(current).root) {
+		if (path.basename(current) === ROOT_FOLDER_NAME) {
+			return current;
+		}
+		current = path.dirname(current);
+	}
+	throw new AppError({
+		statusCode: 500,
+		code: CONFIG_ERRORS.ROOT_FOLDER,
+		message: `Project root ${ROOT_FOLDER_NAME} not found`,
+		handlerName: "getConfig",
+	});
+}
+
+const resolvedRoot = findRoot();
+
+export function getPath(relPath: string): string {
+	const cleanPath = relPath.replace(/^\.?\/*/, ""); // Remove leading './' or '/'
+	return path.resolve(resolvedRoot, cleanPath);
+}
 
 // Load '.env' file (if any)
 // dotenv.config({ path: path.resolve(__dirname, "../../.env") });
@@ -19,29 +46,32 @@ try {
 	// 	path: [path.resolve(__dirname, "../../.env")],
 	// });
 	// const result = dotenv.config({ path: ['.env.local', '.env'] })
+	// const result = dotenv.config({
+	// 	path: [
+	// 		path.resolve(__dirname, "../../../.env"),
+	// 		path.resolve(__dirname, "../../.env"),
+	// 	],
+	// });
 	const result = dotenv.config({
-		path: [
-			path.resolve(__dirname, "../../../.env"),
-			path.resolve(__dirname, "../../.env"),
-		],
+		path: [getPath("./.env"), getPath("./microservices/users/.env")],
 	});
 
-	console.log("[dotenv] config result:", result);
-	console.log(
+	logger.log("[dotenv] config result:", result);
+	logger.log(
 		"(expected: WATA + FACK). process.env.WATA + ",
 		process.env.WATA,
 	);
-	console.log(
+	logger.log(
 		"(expected: MOTHA + FUKA). process.env.MOTHA + ",
 		process.env.MOTHA,
 	);
 	if (result.error) {
-		console.error("[dotenv] Error loading .env:", result.error);
+		logger.error("[dotenv] Error loading .env:", result.error);
 	} else {
-		console.log("[dotenv] Loaded variables:", result.parsed);
+		logger.log("[dotenv] Loaded variables:", result.parsed);
 	}
 } catch (err) {
-	console.error("[dotenv] Threw error:", err);
+	logger.error("[dotenv] Threw error:", err);
 }
 // TODO: END Testing
 
@@ -67,10 +97,10 @@ let cachedConfig: AppConfig | null = null;
 export default function getConfig(): AppConfig {
 	if (cachedConfig) return cachedConfig;
 
-	logger.log("[Config] Loading config...");
+	logger.log("[config] Loading config...");
 
 	const secretsPath = "/run/secrets"; // Docker-style secrets path
-	const fixPackageJsonPath = "../../package.json";
+	const packageJsonPath = "../../package.json";
 	const hasSecretsFolder = fs.existsSync(secretsPath); // For checking if folder exists
 	// Define hardcoded values here
 	const hardcodedDefaults = {
@@ -86,17 +116,17 @@ export default function getConfig(): AppConfig {
 	// or code a function that loads from files (different from loadSecret)
 	const pkg = (() => {
 		try {
-			const pkgPath = path.resolve(__dirname, fixPackageJsonPath);
+			const pkgPath = path.resolve(__dirname, packageJsonPath);
 			return JSON.parse(fs.readFileSync(pkgPath, "utf-8"));
 		} catch (err) {
-			logger.warn("[Config] Could not load package.json:", err);
+			logger.warn("[config] Could not load package.json:", err);
 			return {};
 		}
 	})();
 
 	// Helper function to load from Docker-style secrets
 	const loadSecret = (key: string | undefined): string | undefined => {
-		logger.log("[Config] loadSecret() called for key:", key);
+		logger.log("[config] loadSecret() called for key:", key);
 		if (!key || !hasSecretsFolder) return undefined;
 		const filePath = path.join(secretsPath, key.toLowerCase());
 		try {
@@ -104,49 +134,83 @@ export default function getConfig(): AppConfig {
 				return fs.readFileSync(filePath, "utf-8").trim();
 			}
 		} catch (err) {
-			// Optional:
-			logger.warn(`[Config] Failed to read secret ${key}:`, err);
+			logger.warn(`[config] Failed to read secret ${key}:`, err);
+			// Optional: throw an error?
+			// throw new AppError({
+			// 	statusCode: 500,
+			// 	code: CONFIG_ERRORS.NOT_FOUND,
+			// 	message: `[config] Failed to read secret ${key}:`,
+			// 	handlerName: "getConfig",
+			// });
 		}
 		return undefined;
 	};
 
+	function resolveConfigValue<T>(
+		key: string,
+		sources: [string, T | undefined | null][],
+	): T {
+		for (const [sourceName, value] of sources) {
+			if (value != null) {
+				logger.log(
+					`[config] ${key} = ${value} Loaded from ${sourceName}`,
+				);
+				return value;
+			}
+		}
+		logger.warn(`[config] ${key} is missing in all sources`);
+		// Optional: throw an error?
+		throw new AppError({
+			statusCode: 500,
+			code: CONFIG_ERRORS.NOT_FOUND,
+			message: `[config] ${key} is missing in all sources`,
+			handlerName: "getConfig",
+		});
+	}
+
 	// Loads config in this order (conditional): 1st Docker secret | 2nd environment | 3rd hardcoded values
 	const config: AppConfig = {
-		PAGINATION_ENABLED:
-			loadSecret("PAGINATION_ENABLED") ??
-			process.env.PAGINATION_ENABLED ??
-			hardcodedDefaults.PAGINATION_ENABLED,
+		NODE_ENV: resolveConfigValue("NODE_ENV", [
+			["secret", loadSecret("NODE_ENV")],
+			["env", process.env.NODE_ENV],
+			["default", hardcodedDefaults.NODE_ENV],
+		]),
 
-		DEFAULT_PAGE_SIZE:
-			loadSecret("DEFAULT_PAGE_SIZE") ??
-			process.env.DEFAULT_PAGE_SIZE ??
-			hardcodedDefaults.DEFAULT_PAGE_SIZE,
+		PAGINATION_ENABLED: resolveConfigValue("PAGINATION_ENABLED", [
+			["secret", loadSecret("PAGINATION_ENABLED")],
+			["env", process.env.PAGINATION_ENABLED],
+			["default", hardcodedDefaults.PAGINATION_ENABLED],
+		]),
 
-		APP_VERSION:
-			loadSecret("APP_VERSION") ??
-			process.env.APP_VERSION ??
-			pkg.version ??
-			hardcodedDefaults.APP_VERSION,
+		DEFAULT_PAGE_SIZE: resolveConfigValue("DEFAULT_PAGE_SIZE", [
+			["secret", loadSecret("DEFAULT_PAGE_SIZE")],
+			["env", process.env.DEFAULT_PAGE_SIZE],
+			["default", hardcodedDefaults.DEFAULT_PAGE_SIZE],
+		]),
 
-		APP_NAME:
-			loadSecret("APP_NAME") ??
-			process.env.APP_NAME ??
-			pkg.name ??
-			hardcodedDefaults.APP_NAME,
+		APP_VERSION: resolveConfigValue("APP_VERSION", [
+			["secret", loadSecret("APP_VERSION")],
+			["env", process.env.APP_VERSION],
+			["package.json", pkg.version],
+			["default", hardcodedDefaults.APP_VERSION],
+		]),
 
-		APP_DESCRIPTION:
-			loadSecret("APP_DESCRIPTION") ??
-			process.env.APP_DESCRIPTION ??
-			pkg.description ??
-			hardcodedDefaults.APP_DESCRIPTION,
+		APP_NAME: resolveConfigValue("APP_NAME", [
+			["secret", loadSecret("APP_NAME")],
+			["env", process.env.APP_NAME],
+			["package.json", pkg.name],
+			["default", hardcodedDefaults.APP_NAME],
+		]),
 
-		NODE_ENV:
-			loadSecret("NODE_ENV") ??
-			process.env.NODE_ENV ??
-			hardcodedDefaults.NODE_ENV,
+		APP_DESCRIPTION: resolveConfigValue("APP_DESCRIPTION", [
+			["secret", loadSecret("APP_DESCRIPTION")],
+			["env", process.env.APP_DESCRIPTION],
+			["package.json", pkg.description],
+			["default", hardcodedDefaults.APP_DESCRIPTION],
+		]),
 	};
 
-	logger.log("[Config] Final config object:", config);
+	logger.log("[config] Final config object:", config);
 
 	cachedConfig = config; // Cache the config
 	return config;
