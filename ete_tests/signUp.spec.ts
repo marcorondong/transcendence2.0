b697ce0064962543fa8185f4f1600fe80d7b3253
Benjamin Szilas <bszilas@student.42vienna.com>
2025-06-21 13:26:46 +0200

    FIX: dev dependency + button check sequence

--- ete_tests/signUp.spec.ts
+++ ete_tests/signUp.spec.ts
@@ -9,14 +9,30 @@ const email = username + "@example.com";
 const nickname = username + "_nick";
 
 test.describe.serial("sign up test", () => {
-	const fillSignUpForm = async (page, username, password, email, nickname) => {
+	const fillSignUpForm = async (
+		page,
+		username,
+		password,
+		email,
+		nickname,
+	) => {
 		await page.getByRole("link", { name: "Sign up", exact: true }).click();
-		await page.getByRole("textbox", { name: "Your Username" }).fill(username);
-		await page.getByRole("textbox", { name: "Your Password" }).fill(password);
-		await page.getByRole("textbox", { name: "Repeat Password" }).fill(password);
+		await page
+			.getByRole("textbox", { name: "Your Username" })
+			.fill(username);
+		await page
+			.getByRole("textbox", { name: "Your Password" })
+			.fill(password);
+		await page
+			.getByRole("textbox", { name: "Repeat Password" })
+			.fill(password);
 		await page.getByRole("textbox", { name: "Your Email" }).fill(email);
-		await page.getByRole("textbox", { name: "Your Nickname" }).fill(nickname);
-		await page.getByRole("button", { name: "Register new account" }).click();
+		await page
+			.getByRole("textbox", { name: "Your Nickname" })
+			.fill(nickname);
+		await page
+			.getByRole("button", { name: "Register new account" })
+			.click();
 	};
 
 	test("can sign up and sign in automatically", async ({ page }) => {
@@ -28,19 +44,37 @@ test.describe.serial("sign up test", () => {
 
 	test("cant sign up twice with same username", async ({ page }) => {
 		await page.goto(homeUrl);
-		await fillSignUpForm(page, username, password, "asd" + email, "asd" + nickname);
+		await fillSignUpForm(
+			page,
+			username,
+			password,
+			"asd" + email,
+			"asd" + nickname,
+		);
 		await expect(page.getByText("failed to sign up")).toBeVisible();
 	});
 
 	test("cant sign up twice with same email", async ({ page }) => {
 		await page.goto(homeUrl);
-		await fillSignUpForm(page, "asd" + username, password, email, "asd" + nickname);
+		await fillSignUpForm(
+			page,
+			"asd" + username,
+			password,
+			email,
+			"asd" + nickname,
+		);
 		await expect(page.getByText("failed to sign up")).toBeVisible();
 	});
 
 	test("cant sign up twice with same nickname", async ({ page }) => {
 		await page.goto(homeUrl);
-		await fillSignUpForm(page, "asd" + username, password, "asd" + email, nickname);
+		await fillSignUpForm(
+			page,
+			"asd" + username,
+			password,
+			"asd" + email,
+			nickname,
+		);
 		await expect(page.getByText("failed to sign up")).toBeVisible();
 	});
 
@@ -63,12 +97,12 @@ test.describe.serial("sign up test", () => {
 		// delete cookie (sign out)
 		await page.context().clearCookies();
 		await page.reload();
-		await page
-			.getByRole("button", { name: "Sign in", exact: true })
-			.click();
 		await expect(
 			page.getByRole("button", { name: "Sign in", exact: true }),
 		).toBeVisible();
+		await page
+			.getByRole("button", { name: "Sign in", exact: true })
+			.click();
 		await expect(
 			page.getByRole("link", { name: "Sign up", exact: true }),
 		).toBeVisible();
