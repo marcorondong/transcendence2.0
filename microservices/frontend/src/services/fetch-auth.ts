1b1e7dd2489c5ba320409c9b0f8aeb91ff66a386
ismayilguliyev28 <guliyevismayil@gmail.com>
2025-06-24 15:24:43 +0200

    update-jwt and refresh-jwt are added to FetchAuth, FetchAuth.updateJwt() is added patchDetail()

--- microservices/frontend/src/services/fetch-auth.ts
+++ microservices/frontend/src/services/fetch-auth.ts
@@ -62,4 +62,22 @@ export class FetchAuth {
 		};
 		return await fetchPong(config);
 	}
+
+	static async updateJwt() {
+		const config: FetchConfig = {
+			url: "/auth-api/update-jwt",
+			method: "POST",
+			headers: { accept: "application/json" },
+		};
+		return await fetchPong(config);
+	}
+
+	static async refreshJwt() {
+		const config: FetchConfig = {
+			url: "/auth-api/refresh-jwt",
+			method: "POST",
+			headers: { accept: "application/json" },
+		};
+		return await fetchPong(config);
+	}
 }
