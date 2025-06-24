5dfc61d0477cec7f85e06da9d162ca760d150e5b
ismayilguliyev28 <guliyevismayil@gmail.com>
2025-06-24 12:15:39 +0200

    [IMPROVMENT] better globalErrorHandler() for services

--- microservices/pong_db/src/utils/globalErrorHandler.ts
+++ microservices/pong_db/src/utils/globalErrorHandler.ts
@@ -1,4 +1,5 @@
 import type { FastifyError, FastifyReply, FastifyRequest } from "fastify";
+import { ZodError } from "zod";
 
 export function globalErrorHandler(
 	error: FastifyError,
@@ -6,9 +7,26 @@ export function globalErrorHandler(
 	reply: FastifyReply,
 ) {
 	reply.log.error(error);
-	reply.status(503).send({
-		statusCode: 503,
-		error: "Service Unavailable",
-		message: "Service is not available right now. Please try again later.",
+
+	if (error instanceof TypeError && error.message.includes("fetch failed")) {
+		return reply.status(502).send({
+			statusCode: 502,
+			error: "Bad Gateway",
+			message: "Couldn't reach upstream service. Please try again later.",
+		});
+	}
+
+	if (error.validation || error instanceof ZodError) {
+		return reply.status(400).send({
+			statusCode: 400,
+			error: "Bad Request",
+			message: "Bad Request. Please check your input.",
+		});
+	}
+
+	return reply.status(500).send({
+		statusCode: 500,
+		error: "Internal Server Error",
+		message: "Something went wrong. Please try again later.",
 	});
 }
