25d2ca9e8568350bd91055af4c468a7a4f9a4897
ismayilguliyev28 <guliyevismayil@gmail.com>
2025-06-25 00:28:31 +0200

    [PRETTIER]

--- microservices/chat_api/src/webSocketConnection/requests.ts
+++ microservices/chat_api/src/webSocketConnection/requests.ts
@@ -12,14 +12,11 @@ export async function requests(message: string, client: Client) {
 	const raw = JSON.parse(message.toString());
 	const data = DataSchema.parse(raw);
 
-	if (data.type === "message")
-		await messageHandler(data, client);
-	else if (data.type === "invite")
-		await inviteHandler(data, client);
+	if (data.type === "message") await messageHandler(data, client);
+	else if (data.type === "invite") await inviteHandler(data, client);
 	else if (data.type === "updateNickname")
 		await updateNicknameHandler(client);
 	else if (data.type === "refreshFriendList")
 		await refreshFriendListHandler(data, client);
-	else
-		throw new Error("Server: invalid request type");
+	else throw new Error("Server: invalid request type");
 }
